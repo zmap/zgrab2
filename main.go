0a45a6952b31431460cf84a820414aa83b0bcc7a
Alex <ajholland77@yahoo.com>
2017-07-06 15:33:51 -0400

    Add support for multiple protocols, custom parsing

--- main.go
+++ main.go
@@ -1,37 +1,20 @@
 package main
 
 import (
-    "os"
-
-    "gopkg.in/alecthomas/kingpin.v2"
+	"github.com/jessevdk/go-flags"
+	"log"
+	"os"
 )
 
-var (
-    zgrab = kingpin.New("zgrab", "Banner Grabber")
-    outputFile = zgrab.Flag("output-file", "Output filename, use - for stdout").Default("-").String()
-    inputFile = zgrab.Flag("input-file", "Input filename, use - for stdin").Default("-").String()
-    metadataFile = zgrab.Flag("metadata-file", "File to record banner-grab metadata, use - for stdout").Default("-").String()
-    logFile = zgrab.Flag("log-file", "File to log to, use - for stderr").Default("-").String()
-    
-
-    ssh = zgrab.Command("ssh", "SSH scan")
-    sshClient = ssh.Arg("ssh-client", "Mimic behavior of a specific SSH client").String()
-    sshHostKeyAlgorithms = ssh.Arg("ssh-host-key-algorithms", "Set SSH Host Key Algorithms").String()
-    sshKexAlgorithms = ssh.Arg("ssh-key-algorithms", "Set SSH Key Exchange Algorithms").String()
-    tls = zgrab.Command("tls", "TLS scan")
-    http = zgrab.Command("http", "HTTP scan")
-    ftp = zgrab.Command("ftp", "FTP scan")
-)
+var options [10]Options
+var parser = flags.NewParser(&options[0], flags.Default)
 
 func main() {
-    switch kingpin.MustParse(zgrab.Parse(os.Args[1:])) {
-        case ssh.FullCommand():
-            println("ssh scan")
-        case tls.FullCommand():
-            println("tls scan")
-        case http.FullCommand():
-            println("http scan")
-        case ftp.FullCommand():
-            println("ftp scan")
-    }
+	if _, err := parser.Parse(); err != nil {
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			log.Fatal("Error: ", err.Error())
+		}
+	}
 }
