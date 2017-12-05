79845c2c8f18b0e6d7877edc908a3d1eebe80592
Justin Bastress <jbastress@censys.io>
2017-12-05 13:12:51 -0500

    split out TLS handling

--- lib/mysql/mysql.go
+++ lib/mysql/mysql.go
@@ -205,7 +205,7 @@ type HandshakePacket struct {
 	// auth_plugin_data_len: int<1>
 	AuthPluginDataLen byte `json:"auth_plugin_data_len"`
 	// if (capabilities & CLIENT_SECURE_CONNECTION) {
-	// reserved:  string<10> all 0
+	// reserved:  string<10> all 0: custom marshaler will omit this if it is all \x00s.
 	Reserved []byte `json:"reserved,omitempty"`
 	// auth_plugin_data_part_2: string<MAX(13, auth_plugin_data_len - 8)>
 	AuthPluginData2 string `json:"auth_plugin_data_part_2,omitempty"`
@@ -489,7 +489,7 @@ func (c *Connection) readPacket() (PacketInfo, error) {
 	packet.Raw = base64.StdEncoding.EncodeToString(body)
 
 	if seq != c.SequenceNumber {
-		log.Warnf("Sequence number mismatch: got 0x%x, expected 0x%x", seq, c.SequenceNumber+1)
+		log.Debugf("Sequence number mismatch: got 0x%x, expected 0x%x", seq, c.SequenceNumber+1)
 	}
 	// Update sequence number
 	c.SequenceNumber = seq + 1
@@ -504,6 +504,7 @@ func (c *Connection) readPacket() (PacketInfo, error) {
 
 // Perform a TLS handshake using the configured TLSConfig on the current connection
 func (c *Connection) StartTLS() error {
+
 	client := tls.Client(*c.Connection, c.Config.TLSConfig)
 	err := client.Handshake()
 	if err != nil {
@@ -518,21 +519,49 @@ func (c *Connection) IsSecure() bool {
 	return c.TLSHandshake != nil
 }
 
+// Check if both the input client flags and the server capability flags support TLS
+func (c *Connection) SupportsTLS() bool {
+	if c.State != STATE_CONNECTED {
+		// Vacuously false if you are not connected
+		return false
+	}
+	return (c.Handshake.CapabilityFlags & c.Config.ClientCapabilities & CLIENT_SSL) != 0
+}
+
+// Send the SSL_REQUEST packet (the client should begin the TLS handshake immediately after this returns successfully)
+func (c *Connection) NegotiateTLS() error {
+	c.State = STATE_SSL_REQUEST
+	sslRequest := SSLRequestPacket{
+		CapabilityFlags: c.Config.ClientCapabilities,
+		MaxPacketSize:   c.Config.MaxPacketSize,
+		CharacterSet:    c.Config.CharSet,
+		Reserved:        c.Config.ReservedData,
+	}
+	if err := c.sendPacket(&sslRequest); err != nil {
+		return fmt.Errorf("Error sending SSLRequest packet: %s", err)
+	}
+	c.State = STATE_SSL_HANDSHAKE
+	return nil
+}
+
 // Connect to the configured server and perform the initial handshake
 func (c *Connection) Connect() error {
-	// @TODO @FIXME -- Break this into Connect/Scan/Disconnect?
 	// Allow Scan on pre-connected / user-supplied connections?
 	dialer := net.Dialer{Timeout: c.Config.Timeout}
+	log.Debugf("Connecting to %s:%d", c.Config.Host, c.Config.Port)
 	conn, err := dialer.Dial("tcp", fmt.Sprintf("%s:%d", c.Config.Host, c.Config.Port))
 	if err != nil {
+		log.Debugf("Error connecting: %v", err)
 		return fmt.Errorf("Connect error: %s", err)
 	}
 	c.Connection = &conn
 	c.State = STATE_CONNECTED
+	c.Handshake = nil
 	c.TLSHandshake = nil
 
 	packet, err := c.readPacket()
 	if err != nil {
+		log.Debugf("Error reading handshake packet: %v", err)
 		return fmt.Errorf("Error reading server handshake packet: %s", err)
 	}
 
@@ -540,8 +569,10 @@ func (c *Connection) Connect() error {
 	case *HandshakePacket:
 		// OK -- nothing to do
 	case *ERRPacket:
+		log.Debugf("Got error packet: 0x%x / %s", p.ErrorCode, p.ErrorMessage)
 		return fmt.Errorf("Server returned error after connecting: error_code = 0x%x; error_message = %s", p.ErrorCode, p.ErrorMessage)
 	default:
+		log.Debugf("Unexpected response: %v", p)
 		jsonStr, err := json.Marshal(p)
 		if err != nil {
 			return fmt.Errorf("Server returned unexpected packet type, failed to marshal paclet: %s", err)
@@ -551,29 +582,6 @@ func (c *Connection) Connect() error {
 
 	handshakePacket := packet.(*HandshakePacket)
 	c.Handshake = handshakePacket
-
-	// How to handle mismatched reserved? It will be available in the output, but should it trigger a 'failure'?
-	// if hex.EncodeToString(parsed.reserved) != "00000000000000000000" { ... }
-
-	if (handshakePacket.CapabilityFlags & c.Config.ClientCapabilities & CLIENT_SSL) != 0 {
-		c.State = STATE_SSL_REQUEST
-		sslRequest := SSLRequestPacket{
-			CapabilityFlags: c.Config.ClientCapabilities,
-			MaxPacketSize:   c.Config.MaxPacketSize,
-			CharacterSet:    c.Config.CharSet,
-			Reserved:        c.Config.ReservedData,
-		}
-		if c.sendPacket(&sslRequest) != nil {
-			return fmt.Errorf("Error sending SSLRequest packet: %s", err)
-		}
-		c.State = STATE_SSL_HANDSHAKE
-
-		if c.StartTLS() != nil {
-			return fmt.Errorf("Error performing TLS handshake: %s", err)
-		}
-		c.TLSHandshake = (*c.Connection).(*tls.Conn).GetHandshakeLog()
-	}
-	c.State = STATE_FINISHED
 	return nil
 }
 
--- modules/mysql.go
+++ modules/mysql.go
@@ -1,8 +1,10 @@
 package modules
 
 import (
+	"fmt"
+	"net"
+
 	log "github.com/sirupsen/logrus"
-	"github.com/zmap/zcrypto/tls"
 	"github.com/zmap/zgrab2"
 	"github.com/zmap/zgrab2/lib/mysql"
 )
@@ -10,12 +12,13 @@ import (
 // HandshakeLog contains detailed information about each step of the
 // MySQL handshake, and can be encoded to JSON.
 type MySQLScanResults struct {
-	PacketLog    []*mysql.PacketLogEntry `json:"packet_log"`
-	TLSHandshake *tls.ServerHandshake    `json:"tls_handshake,omitempty"`
+	PacketLog    []*mysql.PacketLogEntry   `json:"packet_log,omitempty"`
+	HandshakeLog *zgrab2.ZGrabHandshakeLog `json:"tls_handshake,omitempty"`
 }
 
 type MySQLFlags struct {
 	zgrab2.BaseFlags
+	zgrab2.TLSFlags
 	Verbose bool `long:"verbose" description:"Output additional information, including <TODO: including what?>."`
 }
 
@@ -70,20 +73,57 @@ func (s *MySQLScanner) GetPort() uint {
 	return s.config.Port
 }
 
-func (s *MySQLScanner) Scan(t zgrab2.ScanTarget) (interface{}, error) {
+func (s *MySQLScanner) Scan(t zgrab2.ScanTarget) (_result interface{}, thrown error) {
 	sql := mysql.NewConnection(&mysql.Config{
 		Host: t.IP.String(),
 		Port: uint16(s.config.Port),
 	})
-	err := sql.Connect()
-	defer sql.Disconnect()
-	if err != nil {
-		return nil, err
-	}
-	ret := MySQLScanResults{PacketLog: sql.PacketLog}
-	if sql.IsSecure() {
-		ret.TLSHandshake = sql.TLSHandshake
-	}
 
-	return ret, nil
+	result := MySQLScanResults{}
+	_result = &result
+	defer func() {
+		recovered := recover()
+		if recovered != nil {
+			// Don't clobber an explicitly-thrown error message just bcause there was no panic()
+			log.Infof("Got error scanning %s: %v", s.GetName(), recovered)
+			thrown = recovered.(error)
+		}
+		result.PacketLog = sql.PacketLog
+		if result.PacketLog == nil {
+			if thrown == nil {
+				thrown = fmt.Errorf("Unable to retrieve scan logs")
+			}
+		}
+		// Following the example of the SSH module, allow the possibility of failing while still returning a (perhaps incomplete) log
+	}()
+	defer sql.Disconnect()
+	if err := sql.Connect(); err != nil {
+		panic(err)
+	}
+	if sql.SupportsTLS() {
+		if nerr := sql.NegotiateTLS(); nerr != nil {
+			panic(nerr)
+		}
+		var conn *zgrab2.ZGrabConnection
+		if conn, thrown = s.config.TLSFlags.GetZGrabTLSConnection(sql.Connection); thrown != nil {
+			panic(thrown)
+		}
+		if herr := conn.Handshake(); herr == nil {
+			panic(herr)
+		}
+		// Replace sql.Connection to allow hypothetical future calls to go over the secure connection
+		var netConn net.Conn = conn
+		sql.Connection = &netConn
+		// Works:
+		//	var netConn net.Conn = conn
+		//	sql.Connection = &netConn
+		// Does not work:
+		//	sql.Connection = &conn // (**ZGrabConnection is not *net.Conn)
+		//  sql.Connection = &(conn.(net.Conn)) // (conn is not an interface)
+		//  sql.Connection = conn.Conn // (cannot use conn.Conn (type tls.Conn) as type *net.Conn)
+		//  sql.Connection = &conn.Conn // (cannot use &conn.Conn (type *tls.Conn) as type *net.Conn)
+		//	sql.Connection = &(conn.Conn.conn) // (cannot refer to unexported field or method conn)
+		result.HandshakeLog = conn.GetHandshakeLog()
+	}
+	return _result, thrown
 }
