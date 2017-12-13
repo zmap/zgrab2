e2f5cba53a02334c6dc395d77a34205070207a18
Justin Bastress <jbastress@censys.io>
2017-12-13 16:06:36 -0500

    fix path of binary

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,8 +1,4 @@
 #!/bin/sh -e
-# Run zgrab2 against already-running containers.
-# Currently only supports mysql.
-# Usage: integration_tests.sh [sql-version] [port]
-PATH=/c/Python27:$PATH
 SQL_VERSION="$1"
 PORT="$2"
 CONTAINER_NAME="testmysql-$SQL_VERSION"
@@ -13,10 +9,10 @@ shift
 shift
 
 echo "Testing MySQL Version $SQL_VERSION on port $PORT..."
-echo "127.0.0.1" | ./zgrab2 mysql -p $PORT $* > $OUTPUT_FILE
+echo "127.0.0.1" | ./cmd/zgrab2/zgrab2 mysql -p $PORT $* > $OUTPUT_FILE
 
-if [ -d $ZSCHEMA_PATH ]; then
-  PYTHONPATH=$ZSCHEMA_PATH /c/Python27/python.exe -m zschema validate schemas/__init__.py:zgrab2 $OUTPUT_FILE
+if [ -d zschema ]; then
+  PYTHONPATH=$PYTHONPATH:$ZSCHEMA_PATH python -m zschema validate schemas/__init__.py:zgrab2 $OUTPUT_FILE
 else
   echo "Skipping schema validation: clone zschema into $ZSCHEMA_PATH to enable"
 fi
