f38d85b71c70b0c6f1c73c4cfeb37852d5c52e41
Andrew Sardone <andrew@andrewsardone.com>
2017-12-18 00:56:57 -0500

    Add script to generate new-protocol tests [minor]
    Just a minor little utility script for generating shell scripts around
    the setup/test/cleanup rhythm of the zgrab integration tests.

---
+++ integration_tests/new.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Utility script for scaffolding stub test files for a new protocol
+
+# Run from root of project
+TEST_DIR=$(dirname "$0")
+cd "$TEST_DIR/.."
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: ./integration_tests/new.sh <new_protocol_name>"
+  exit 1
+fi
+
+module_name="$1"
+module_path="integration_tests/$module_name"
+
+mkdir -p $module_path
+
+cat << EOF > $module_path/setup.sh
+#!/usr/bin/env bash
+
+echo "Tests setup for $module_name"
+EOF
+chmod +x $module_path/setup.sh
+
+cat << EOF > $module_path/test.sh
+#!/usr/bin/env bash
+
+echo "Tests runner for $module_name"
+EOF
+chmod +x $module_path/test.sh
+
+cat << EOF > $module_path/cleanup.sh
+#!/usr/bin/env bash
+
+echo "Tests cleanup for $module_name"
+EOF
+chmod +x $module_path/cleanup.sh
+
+echo "Test files scafollded in $module_path"
--- integration_tests/test.sh
+++ integration_tests/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # Do all integration tests for all protocols
-# To add tests for a new protocol, create a directory integration_tests/<new_protocol>, and drop its setup.sh, test.sh, and cleanup.sh there.
+# To add tests for a new protocol, run `./integration_tests/new.sh <new_protocol>` and implement the appropriate test scripts.
 
 # Run from root of project
 TEST_DIR=$(dirname "$0")
