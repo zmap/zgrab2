44810b8389857a7f349133c0d6d1a1ce7d945717
Justin Bastress <jbastress@censys.io>
2017-12-21 17:33:57 -0500

    cleanup should actually stop the process though

--- integration_tests/postgres/cleanup.sh
+++ integration_tests/postgres/cleanup.sh
@@ -13,7 +13,7 @@ function clean() {
   # TODO HACK FIXME: Hack for MinGW: for some reason, /var/lib/ is being replaced with c:/mingw/msys/1.0/var/lib/, but //var/lib is safe.
   docker exec -t $CONTAINER_NAME cat //var/lib/postgresql/data/pg_log/postgres.log
   echo ")}] END POSTGRES LOGS FROM $CONTAINER_NAME"
-  # docker stop $CONTAINER_NAME
+  docker stop $CONTAINER_NAME
 }
 
 versions="9.3 9.4 9.5 9.6 10.1"
