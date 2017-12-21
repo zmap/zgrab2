43edfddd404bbceff4f7825b0c902c9eeb212d88
Justin Bastress <jbastress@censys.io>
2017-12-21 17:33:36 -0500

    hack for cleanup.sh to work on mingw -- use //var/lib instead of /var/lib

--- integration_tests/postgres/cleanup.sh
+++ integration_tests/postgres/cleanup.sh
@@ -10,9 +10,10 @@ function clean() {
   docker logs --tail all $CONTAINER_NAME
   echo ")}] END DOCKER LOGS FROM $CONTAINER_NAME"
   echo "BEGIN POSTGRES LOGS FROM $CONTAINER_NAME [{("
-  docker exec -t $CONTAINER_NAME cat /var/lib/postgresql/data/pg_log/postgres.log
+  # TODO HACK FIXME: Hack for MinGW: for some reason, /var/lib/ is being replaced with c:/mingw/msys/1.0/var/lib/, but //var/lib is safe.
+  docker exec -t $CONTAINER_NAME cat //var/lib/postgresql/data/pg_log/postgres.log
   echo ")}] END POSTGRES LOGS FROM $CONTAINER_NAME"
-  docker stop $CONTAINER_NAME
+  # docker stop $CONTAINER_NAME
 }
 
 versions="9.3 9.4 9.5 9.6 10.1"
