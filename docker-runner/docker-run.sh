3e9f2067acde055e78a2f59c3b17f0b5beb83edf
Andrew Sardone <andrew@andrewsardone.com>
2018-01-03 10:36:41 -0500

    Use param expansion to check for env variable [minor]
    This is a *very* minor change to `docker-runner/docker-run.sh` checks to
    see if the environment variable required to run the script has been set
    to a non-empty string. If not, the script exits with a non-zero status
    code and displays a default message:
    
    ```
    ❯ docker-runner/docker-run.sh
    docker-runner/docker-run.sh: line 7: CONTAINER_NAME: parameter null or not set
    ```
    
    This was the behavior before, but just uses a one-liner declarative bash
    idiom.
    
    For further reading on parameter expansion, see
    https://stackoverflow.com/a/307735.
    
    @justinbastress can tell me if I did something wrong and broke the
    intent of the script :-)

--- docker-runner/docker-run.sh
+++ docker-runner/docker-run.sh
@@ -4,10 +4,7 @@
 # Links the runner image to the targetted container with the hostname alias "target",
 # then scans target using the arguments to the script.
 
-if [ -x $CONTAINER_NAME ]; then
-  echo "docker-run.sh: Must provide CONTAINER_NAME environment variable"
-  exit 1
-fi
+: "${CONTAINER_NAME:?}"
 
 set -e
 docker run --link $CONTAINER_NAME:target -e ZGRAB_TARGET=target zgrab2_runner $@
