130b30d15e66a89ae0502d8a46c8de088dda5235
Justin Bastress <jbastress@censys.io>
2018-01-08 15:41:15 -0500

    use zgrab2_service_base for lighttpd container; restart http container in http/setup.sh

--- integration_tests/http/setup.sh
+++ integration_tests/http/setup.sh
@@ -5,6 +5,12 @@ set -e
 CONTAINER_TAG="zgrab_http"
 CONTAINER_NAME="zgrab_http"
 
+if docker ps --filter "name=$CONTAINER_NAME" | grep $CONTAINER_NAME; then
+  echo "http/setup: Container $CONTAINER_NAME already running -- stopping..."
+  docker stop $CONTAINER_NAME
+  echo "...stopped."
+fi
+
 # First attempt to just launch the container
 if ! docker run --rm --name $CONTAINER_NAME -itd $CONTAINER_TAG; then
     # If it fails, build it from ./container/Dockerfile
