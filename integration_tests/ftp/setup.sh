6104075d50284f0f45bfdb6d21acc172fd5c9372
Justin Bastress <jbastress@censys.io>
2018-01-08 15:54:46 -0500

    ftp: use zgrab2_service_base for ftp container; restart container if already running

--- integration_tests/ftp/setup.sh
+++ integration_tests/ftp/setup.sh
@@ -7,7 +7,13 @@ CONTAINER_NAME="zgrab_ftp"
 
 # TODO FIXME: find a pre-built container with ftpd already running? This works, but if it has to build the container image, the apt-get update can be very slow.
 
-# First attempt to just launch the container
+if docker ps --filter "name=$CONTAINER_NAME" | grep $CONTAINER_NAME; then
+  echo "ftp/setup: Container $CONTAINER_NAME already running -- stopping..."
+  docker stop $CONTAINER_NAME
+  echo "...stopped."
+fi
+
+# First attempt: just launch the container
 if ! docker run --rm --name $CONTAINER_NAME -itd $CONTAINER_TAG; then
     # If it fails, build it from ./container/Dockerfile
     docker build -t $CONTAINER_TAG ./container
