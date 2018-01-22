c1d2b078560ad1acde675768a42f63887f60ab53
Justin Bastress <jbastress@censys.io>
2018-01-22 14:16:13 -0500

    initial work on NTP

--- modules/ntp.go
+++ modules/ntp.go
@@ -1,10 +1,9 @@
 package modules
 
-import "github.com/jb/tcpwrap"
-
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"time"
 
@@ -40,108 +39,131 @@ const (
 	Private                          = 7
 )
 
-// NTPFlags = LeapIndicator (2 bits) + Version (3 bits) + Mode (3 bits)
-// Defined in https://tools.ietf.org/html/rfc5905 section 7.3
-type NTPFlags uint8
+// Constants from ntp/include/ntp_request.h
+const (
+	IMPL_UNIV uint8 = 0
+	IMPL_XNTPD_OLD = 2
+	IMPL_XNTPD = 3
+)
 
-func (self *NTPFlags) SetLeap(val LeapIndicator) {
-	*self = NTPFlags((uint8(*self) & 0x3F) | uint8(val<<6))
+const (
+	REQ_PEER_LIST uint8 = 0
+	REQ_MON_GETLIST_1  = 42
+)
+
+// NTPShort a 32-bit struct defined in figure 3. The upper 16 bits are the seconds, the lower 16 bits are the fractional seconds.
+type NTPShort struct {
+	Seconds uint16 `json:"seconds"`
+	Fraction uint16 `json:"fraction"`
 }
 
-func (self *NTPFlags) GetLeap() LeapIndicator {
-	return LeapIndicator(*self >> 6)
+func (self *NTPShort) Decode(buf []byte) error {
+	if len(buf) < 4 {
+		return ErrBufferTooSmall
+	}
+	self.Seconds = binary.BigEndian.Uint16(buf[0:2])
+	self.Fraction = binary.BigEndian.Uint16(buf[2:4])
+	return nil  
 }
 
-func (self *NTPFlags) GetVersion() uint8 {
-	return (uint8(*self) & 0x3F) >> 3
+func DecodeNTPShort(buf []byte) (*NTPShort, error) {
+	if len(buf) < 4 {
+		return nil, ErrBufferTooSmall
+	}
+	ret := NTPShort{}
+	err := ret.Decode(buf)
+	return &ret, err
 }
 
-func (self *NTPFlags) SetVersion(val uint8) {
-	*self = NTPFlags((uint8(*self) & 0xC7) | (val << 3))
-}
-
-func (self *NTPFlags) GetMode() AssociationMode {
-	return AssociationMode(uint8(*self) & 0x07)
-}
-
-func (self *NTPFlags) SetMode(val AssociationMode) {
-	*self = NTPFlags((uint8(*self) & 0xF8) | uint8(val))
-}
-
-// NTPShort a 32-bit fixed-length number defined in figure 3. The upper 16 bits are the seconds, the lower 16 bits are the fractional seconds.
-type NTPShort uint32
-
-func (self *NTPShort) GetSeconds() uint16 {
-	return uint16(*self >> 16)
-}
-
-func (self *NTPShort) GetFraction() uint16 {
-	return uint16(*self & 0x0000FFFF)
+func (self *NTPShort) Encode() []byte {
+	ret := make([]byte, 4)
+	binary.BigEndian.PutUint16(ret[0:2], self.Seconds)
+	binary.BigEndian.PutUint16(ret[2:4], self.Fraction)
+	return ret
 }
 
 func (self *NTPShort) GetNanos() uint32 {
 	// frac/2^16 = nanos/10^9
 	// nanos = frac * 10^9 / 2^16
-	return uint32(float32(self.GetFraction()) / float32(1<<16) * 1e9)
+	return uint32(float32(self.Fraction) / float32(1<<16) * 1e9)
 }
 
-func (self *NTPShort) SetSeconds(val uint16) {
-	*self = NTPShort((uint32(*self) & 0x0000FFFF) | uint32(val)<<16)
-}
-
-func (self *NTPShort) SetFraction(val uint16) {
-	*self = NTPShort((uint32(*self) & 0xFFFF0000) | uint32(val))
+func (self *NTPShort) SetNanos(nanos int) {
+	frac := float32(nanos) / float32(1e9) * 0x10000
+	self.Fraction = uint16(frac)
 }
 
 func (self *NTPShort) GetDuration() time.Duration {
-	return time.Duration(self.GetSeconds())*time.Second + time.Duration(self.GetNanos())*time.Nanosecond
+	return time.Duration(self.Seconds)*time.Second + time.Duration(self.GetNanos())*time.Nanosecond
 }
 
 func (self *NTPShort) SetDuration(d time.Duration) {
 	ns := d.Nanoseconds()
-	self.SetSeconds(uint16(ns / 1e9))
-	frac := float32(ns%1e9) / float32(1e9) * 0x10000
-	self.SetFraction(uint16(frac))
+	self.Seconds = uint16(ns / 1e9)
+	self.SetNanos(int(ns % 1e9))
 }
 
 // NTPLong a 64-bit fixed-length number defined in figure 3. The upper 32 bits are the seconds, the lower 32 bits are the fractional seconds.
-type NTPLong uint64
-
-func (self *NTPLong) GetSeconds() uint32 {
-	return uint32(*self >> 32)
-}
-
-func (self *NTPLong) GetFraction() uint32 {
-	return uint32(*self & 0x000000FFFFFFFF)
-}
-
-func (self *NTPLong) SetSeconds(val uint32) {
-	*self = NTPLong((uint64(*self) & uint64(0x000000FFFFFFFF)) | (uint64(val) << 32))
-}
-
-func (self *NTPLong) SetFraction(val uint32) {
-	*self = NTPLong((uint64(*self) & uint64(0xFFFFFFFF00000000)) | uint64(val))
+type NTPLong struct {
+	Seconds uint32 `json:"seconds"`
+	Fraction uint32 `json:"fraction"`
 }
 
 func (self *NTPLong) GetNanos() uint64 {
-	return uint64(float64(self.GetFraction()) / float64(1<<32) * 1e9)
+	return uint64(float64(self.Fraction) / float64(1<<32) * 1e9)
+}
+
+func (self *NTPLong) SetNanos(nanos int) {
+	frac := float64(nanos) / float64(10^9) * (1 << 32)
+	self.Fraction = uint32(frac)
 }
 
 func (self *NTPLong) GetTime() time.Time {
-	return NTPEpoch.Add(time.Duration(self.GetSeconds())*time.Second + time.Duration(self.GetNanos())*time.Nanosecond)
+	return NTPEpoch.Add(time.Duration(self.Seconds)*time.Second + time.Duration(self.GetNanos())*time.Nanosecond)
 }
 
 func (self *NTPLong) SetTime(t time.Time) {
 	ntpTime := t.Add(UnixEpoch.Sub(NTPEpoch))
 	s := ntpTime.Unix()
 	ns := ntpTime.UnixNano() - s*1e9
-	self.SetSeconds(uint32(s))
-	self.SetFraction(uint32(float64(ns) / float64(10^9) * (1 << 32)))
+	self.Seconds = uint32(s)
+	self.SetNanos(int(ns))
+}
+
+func (self *NTPLong) Decode(buf []byte) error {
+	if len(buf) < 8 {
+		return ErrBufferTooSmall
+	}
+	self.Seconds = binary.BigEndian.Uint32(buf[0:4])
+	self.Fraction = binary.BigEndian.Uint32(buf[4:8])
+	return nil  
+}
+
+func DecodeNTPLong(buf []byte) (*NTPLong, error) {
+	if len(buf) < 8 {
+		return nil, ErrBufferTooSmall
+	}
+	ret := NTPLong{}
+	err := ret.Decode(buf)
+	return &ret, err
+}
+
+func (self *NTPLong) Encode() []byte {
+	ret := make([]byte, 8)
+	binary.BigEndian.PutUint32(ret[0:4], self.Seconds)
+	binary.BigEndian.PutUint32(ret[4:8], self.Fraction)
+	return ret
 }
 
 type NTPHeader struct {
-	// Flags contains the LeapIndicator, Version, and Mode.
-	Flags NTPFlags `json:"flags"`
+	// LeapIndicator is the the top two bits of the first byte
+	LeapIndicator LeapIndicator `json:"leap_indicator"`
+
+	// Version is bits 5..3 of the first byte
+	Version uint8 `json:"version"`
+
+	// The mode is the lowest three bits of the first byte
+	Mode AssociationMode `json:"mode"`
 
 	// Stratum is defined in figure 11: values > 16 are reserved
 	Stratum uint8 `json:"stratum"`
@@ -175,16 +197,88 @@ type NTPHeader struct {
 	TransmitTimestamp NTPLong `json:"transmit_timestamp,omitempty"`
 }
 
+var ErrInvalidLeapIndicator = fmt.Errorf("The leap indicator was not valid")
 var ErrInvalidVersion = fmt.Errorf("The version number was not valid")
 var ErrInvalidMode = fmt.Errorf("The mode was 0")
 var ErrInvalidStratum = fmt.Errorf("The stratum was invalid")
 var ErrInvalidReferenceID = fmt.Errorf("The reference ID contained non-ASCII characters")
+var ErrBufferTooSmall = fmt.Errorf("The buffer is too small")
+var ErrInvalidHeader = fmt.Errorf("Invalid header data")
+
+func DecodeNTPHeader(buf []byte) (*NTPHeader, error) {
+	if len(buf) < 48 {
+		return nil, ErrBufferTooSmall
+	}
+	ret := NTPHeader{}
+	ret.LeapIndicator = LeapIndicator(buf[0] >> 6)
+	ret.Version = uint8(buf[0] >> 3 & 0x07)
+	ret.Mode = AssociationMode(buf[0] & 0x07)
+	ret.Stratum = uint8(buf[1])
+	ret.Poll = int8(buf[2])
+	ret.Precision = int8(buf[3])
+	if err := ret.RootDelay.Decode(buf[4:8]); err != nil {
+		return nil, err
+	}
+	if err := ret.RootDispersion.Decode(buf[8:12]); err != nil {
+		return nil, err
+	}
+	copy(ret.ReferenceID[:], buf[12:16])
+	if err := ret.ReferenceTimestamp.Decode(buf[16:24]); err != nil {
+		return nil, err
+	}
+	if err := ret.OriginTimestamp.Decode(buf[24:32]); err != nil {
+		return nil, err 
+	}
+	if err := ret.ReceiveTimestamp.Decode(buf[32:40]); err != nil {
+		return nil, err
+	}
+	if err := ret.TransmitTimestamp.Decode(buf[40:48]); err != nil {
+		return nil, err
+	}
+	
+	return &ret, nil
+}
+
+func ReadNTPHeader(conn net.Conn) (*NTPHeader, error) {
+	buf := make([]byte, 48)
+	_, err := io.ReadFull(conn, buf)
+	if err != nil {
+		return nil, err
+	}
+	return DecodeNTPHeader(buf)
+}
+
+func (self *NTPHeader) Encode() ([]byte, error) {
+	ret := make([]byte, 48)
+	if (self.Version >> 3) != 0 {
+		return nil, ErrInvalidVersion
+	}
+	if (self.Mode >> 3) != 0 {
+		return nil, ErrInvalidMode
+	}
+	if (self.LeapIndicator >> 2) != 0 {
+		return nil, ErrInvalidLeapIndicator
+	}
+	ret[0] = byte((uint8(self.LeapIndicator) << 6) | (uint8(self.Mode) << 3) | uint8(self.Version)) 
+	ret[1] = byte(self.Stratum)
+	ret[2] = byte(self.Poll)
+	ret[3] = byte(self.Precision)
+	copy(ret[4:8], self.RootDelay.Encode())
+	copy(ret[8:12], self.RootDispersion.Encode())
+	copy(ret[12:16], self.ReferenceID[:])
+	copy(ret[16:24], self.ReferenceTimestamp.Encode())
+	copy(ret[24:32], self.OriginTimestamp.Encode())
+	copy(ret[32:40], self.ReceiveTimestamp.Encode())
+	copy(ret[40:48], self.TransmitTimestamp.Encode())
+
+	return ret[:], nil
+}
 
 func (self *NTPHeader) ValidateSyntax() error {
-	if self.Flags.GetVersion() < 3 || self.Flags.GetVersion() > 4 {
+	if self.Version < 2 || self.Version > 4 {
 		return ErrInvalidVersion
 	}
-	if self.Flags.GetMode() == 0 {
+	if self.Mode == 0 {
 		return ErrInvalidMode
 	}
 	if self.Stratum > 16 {
@@ -201,6 +295,86 @@ func (self *NTPHeader) ValidateSyntax() error {
 		}
 	}
 	return nil
+}
+
+type PrivatePacketHeader struct {
+	IsResponse bool `json:"is_response"`
+	HasMore bool `json:"has_more"`
+	Version uint8 `json:"version"`
+	Mode uint8 `json:"version"`
+	IsAuthenticated bool `json:"is_authenticated"`
+	SequenceNumber uint8 `json:"sequence_number"`
+	ImplementationNumber uint8 `json:"implementation_number"`
+	RequestCode uint8 `json:"request_code"`
+	Error uint8 `json:"error"`
+	NumRecords uint16 `json:"num_records"`
+	RecordSize uint16 `json:"record_size"`
+	MBZ uint8 `json:"mbz"`
+}
+
+func (self *PrivatePacketHeader) Encode() ([]byte, error) {
+	ret := [8]byte{}
+	if (self.Mode >> 3) != 0 || (self.Version >> 3) != 0 {
+		return nil, ErrInvalidHeader
+	}
+	ret[0] = self.Mode | (self.Version << 3)
+	if self.IsResponse {
+		ret[0] = ret[0] | 0x80
+	}
+	if self.HasMore {
+		ret[0] = ret[0] | 0x40
+	}
+	if self.SequenceNumber & 0x80 != 0 {
+		return nil, ErrInvalidHeader
+	}
+	ret[1] = self.SequenceNumber
+	if self.IsAuthenticated {
+		ret[1] = ret[1] | 0x80
+	}
+	ret[2] = self.ImplementationNumber
+	ret[3] = self.RequestCode
+	if (self.Error >> 4) != 0 || (self.NumRecords >> 12) != 0 {
+		return nil, ErrInvalidHeader
+	}
+	ret[4] = (self.Error << 4) | uint8(self.NumRecords >> 8)
+	ret[5] = byte(self.NumRecords & 0xFF)
+	if (self.MBZ >> 4) != 0  || (self.RecordSize >> 12) != 0 {
+		return nil, ErrInvalidHeader
+	}
+	ret[6] = (self.MBZ << 4) | uint8(self.RecordSize >> 8)
+	ret[7] = byte(self.RecordSize & 0xFF)
+	return ret[:], nil
+}
+
+func ReadPrivateModeHeader(buf []byte) (*PrivatePacketHeader, error) {
+	ret := PrivatePacketHeader{}
+	if len(buf) < 8 {
+		return nil, ErrInvalidHeader
+	}
+	ret.Mode = buf[0] & 0x07
+	ret.Version = buf[0] >> 3 & 0x07
+	ret.IsResponse = (buf[0] >> 6) & 1 == 1 
+	ret.HasMore = (buf[0] >> 7) & 1 == 1
+	ret.SequenceNumber = buf[1] & 0x7F
+	ret.IsAuthenticated = (buf[1] >> 7) & 1 == 1
+	ret.ImplementationNumber = buf[2]
+	ret.RequestCode = buf[3]
+	ret.Error = buf[4] >> 4
+	ret.NumRecords = uint16(buf[4] & 0x0F) << 4 | uint16(buf[5])
+	ret.MBZ = buf[6] >> 4
+	ret.RecordSize = uint16(buf[6] & 0x0f) << 4 | uint16(buf[7])
+	return &ret, nil
+}
+
+func NewMode7Packet(impl uint8, req uint8) *PrivatePacketHeader {
+	return &PrivatePacketHeader{
+		Version: 2,
+		Mode: 7,
+		SequenceNumber: 0x00,
+		ImplementationNumber: impl,
+		RequestCode: req,
+		Error: 0x00,
+	}
 }
 
 type NTPResults struct {
@@ -285,20 +459,21 @@ func (self *NTPScanner) Scan(t zgrab2.ScanTarget) (zgrab2.ScanStatus, interface{
 	if err != nil {
 		return zgrab2.TryGetScanStatus(err), nil, err
 	}
-	sock = tcpwrap.Wrap(sock)
 	outPacket := NTPHeader{}
-	outPacket.Flags.SetMode(Client)
-	outPacket.Flags.SetVersion(3)
-	outPacket.Flags.SetLeap(Unknown)
+	outPacket.Mode = Client
+	outPacket.Version = 3
+	outPacket.LeapIndicator = Unknown
 	outPacket.Stratum = 0
-
-	err = binary.Write(sock, binary.BigEndian, &outPacket)
+	encoded, err := outPacket.Encode()
+	if err != nil {
+		return zgrab2.SCAN_UNKNOWN_ERROR, nil, err
+	}
+	_, err = sock.Write(encoded)
 	if err != nil {
 		return zgrab2.TryGetScanStatus(err), nil, err
 	}
 
-	inPacket := NTPHeader{}
-	err = binary.Read(sock, binary.BigEndian, &inPacket)
+	inPacket, err := ReadNTPHeader(sock)
 	if err != nil {
 		return zgrab2.TryGetScanStatus(err), nil, err
 	}
@@ -310,8 +485,8 @@ func (self *NTPScanner) Scan(t zgrab2.ScanTarget) (zgrab2.ScanStatus, interface{
 	result := &NTPResults{}
 	temp := inPacket.ReceiveTimestamp.GetTime()
 	result.Time = &temp
-	vTemp := inPacket.Flags.GetVersion()
+	vTemp := inPacket.Version
 	result.Version = &vTemp
 
-	return zgrab2.SCAN_SUCCESS, result, nil
+	return zgrab2.SCAN_SUCCESS, &inPacket, nil
 }
