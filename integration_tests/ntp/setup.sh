1233a5e0b06287eb5912fb982b8b0008d8a4380b
Justin Bastress <jbastress@censys.io>
2018-01-23 16:46:50 -0500

    Add tests for partial / application-error returns

--- integration_tests/ntp/setup.sh
+++ integration_tests/ntp/setup.sh
@@ -6,24 +6,24 @@ versions="openntp 4.2.6"
 
 CONTAINER_TAG="zgrab_ntp"
 for version in $versions; do
-  CONTAINER_NAME="zgrab_ntp_$version"
+    CONTAINER_NAME="zgrab_ntp_$version"
 
-  echo "ntp/setup: Setting up $CONTAINER_NAME"
+    echo "ntp/setup: Setting up $CONTAINER_NAME"
 
-  DOCKER_RUN_ARGS="--privileged -itd --rm --name $CONTAINER_NAME"
+    DOCKER_RUN_ARGS="--privileged -itd --rm --name $CONTAINER_NAME"
 
-  if docker ps --filter "name=$CONTAINER_NAME" | grep -q $CONTAINER_NAME; then
-    echo "ntp/setup: Container $CONTAINER_NAME already running -- stopping..."
-    docker stop $CONTAINER_NAME
-    echo "...stopped."
-  fi
+    if docker ps --filter "name=$CONTAINER_NAME" | grep -q $CONTAINER_NAME; then
+        echo "ntp/setup: Container $CONTAINER_NAME already running -- stopping..."
+        docker stop $CONTAINER_NAME
+        echo "...stopped."
+    fi
 
-  # First attempt: just launch the container
-  if ! docker run $DOCKER_RUN_ARGS "$CONTAINER_TAG:$version"; then
-      # If it fails, build it from ./container/Dockerfile
-      echo "ntp/setup: Building $CONTAINER_TAG:$version from ./container-$version"
-      docker build -t "$CONTAINER_TAG:$version" ./container-$version
-      # Try again
-      docker run $DOCKER_RUN_ARGS $CONTAINER_TAG:$version
-  fi
+    # First attempt: just launch the container
+    if ! docker run $DOCKER_RUN_ARGS "$CONTAINER_TAG:$version"; then
+        # If it fails, build it from ./container/Dockerfile
+        echo "ntp/setup: Building $CONTAINER_TAG:$version from ./container-$version"
+        docker build -t "$CONTAINER_TAG:$version" ./container-$version
+        # Try again
+        docker run $DOCKER_RUN_ARGS $CONTAINER_TAG:$version
+    fi
 done
--- integration_tests/ntp/test.sh
+++ integration_tests/ntp/test.sh
@@ -5,6 +5,7 @@ MODULE_DIR=$(dirname $0)
 TEST_ROOT=$MODULE_DIR/..
 ZGRAB_ROOT=$MODULE_DIR/../..
 ZGRAB_OUTPUT=$ZGRAB_ROOT/zgrab-output
+
 OUTPUT_ROOT=$ZGRAB_OUTPUT/ntp
 
 mkdir -p $OUTPUT_ROOT
@@ -15,6 +16,37 @@ function test_openntp() {
     echo "ntp/test: Tests runner for ntp_openntp"
 
     CONTAINER_NAME="zgrab_ntp_openntp" $ZGRAB_ROOT/docker-runner/docker-run.sh ntp > "$OUTPUT_ROOT/openntp.json"
+    
+    # Don't drop this in the standard output root, since it will not have status = success
+    CONTAINER_NAME="zgrab_ntp_openntp" $ZGRAB_ROOT/docker-runner/docker-run.sh ntp --monlist > out.tmp
+    time=$($ZGRAB_ROOT/jp -u data.ntp.result.time < out.tmp)
+    version=$($ZGRAB_ROOT/jp -u data.ntp.result.version < out.tmp)
+    rm -f out.tmp
+    if [ $time = "null" ]; then
+        echo "ntp/test: Failed to get partial result from monlist on openntp (time = null)"
+        exit 1
+    fi
+    if ! [ $version = "3" ]; then
+        echo "ntp/test: Failed to get partial result from monlist on openntp (version = $version)"
+        exit 1
+    fi
+}
+
+function test_bad_req() {
+    code=$1
+    expected_error=$2
+    CONTAINER_NAME="zgrab_ntp_4.2.6" $ZGRAB_ROOT/docker-runner/docker-run.sh ntp --monlist --request-code $code --skip-get-time > out.tmp
+    status=$($ZGRAB_ROOT/jp -u data.ntp.status < out.tmp)
+    error=$($ZGRAB_ROOT/jp -u data.ntp.error < out.tmp)
+    rm -f out.tmp
+    if ! [ $status = "application-error" ]; then
+        echo "ntp/test: Got error '$error', expected '$expected_error' on $code"
+        exit 1
+    fi
+    if ! [ $error = $expected_error ]; then
+        echo "ntp/test: Got error '$error', expected '$expected_error' on $code"
+        exit 1
+    fi
 }
 
 function test_4_2_6() {
@@ -25,10 +57,16 @@ function test_4_2_6() {
     CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh ntp > "$OUTPUT_ROOT/4.2.6_normal.json"
     CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh ntp --monlist > "$OUTPUT_ROOT/4.2.6_monlist.json"
 
-    request_codes="REQ_PEER_LIST REQ_DO_DIRTY_HACK REQ_DONT_DIRTY_HACK REQ_MON_GETLIST_1 REQ_MON_GETLIST"
+    success_codes="REQ_MON_GETLIST_1 REQ_MON_GETLIST"
     for code in $request_codes; do
         CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh ntp --monlist --request-code $code > "$OUTPUT_ROOT/4.2.6_$code.json"
+        CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh ntp --monlist --request-code $code --skip-get-time > "$OUTPUT_ROOT/4.2.6_$code_solo.json"
     done
+
+    # Check that when the server returns with a valid error code that we return status = application-error and we forward the INFO_ERR code from the server
+    test_bad_req "REQ_PEER_LIST" "INFO_ERR_NODATA"
+    test_bad_req "REQ_DO_DIRTY_HACK" "INFO_ERR_REQ"
+    test_bad_req "REQ_DONT_DIRTY_HACK" "INFO_ERR_REQ"
 }
 
 test_openntp
