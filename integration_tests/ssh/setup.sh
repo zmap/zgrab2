e6739acff9cf49491411f6a32cef824c21fe57a3
Justin Bastress <jbastress@censys.io>
2018-01-29 16:53:50 -0500

    fix service test scripts -- remove useless i flags to docker run; if container is already running, use it; smarter wait conditions for postgres and mysql

--- integration_tests/ssh/setup.sh
+++ integration_tests/ssh/setup.sh
@@ -7,16 +7,15 @@ CONTAINER_NAME="zgrab_ssh"
 
 # TODO FIXME: find a pre-built container with sshd already running? This works, but if it has to build the container image, the apt-get update is very slow.
 
-if docker ps --filter "name=$CONTAINER_NAME" | grep $CONTAINER_NAME; then
-  echo "ssh/setup: Container $CONTAINER_NAME already running -- stopping..."
-  docker stop $CONTAINER_NAME
-  echo "...stopped."
+if docker ps --filter "name=$CONTAINER_NAME" | grep -q $CONTAINER_NAME; then
+  echo "ssh/setup: Container $CONTAINER_NAME already running -- nothing to do."
+  exit 0
 fi
 
 # First attempt to just launch the container
-if ! docker run --rm --name $CONTAINER_NAME -itd $CONTAINER_TAG; then
+if ! docker run --rm --name $CONTAINER_NAME -td $CONTAINER_TAG; then
     # If it fails, build it from ./container/Dockerfile
     docker build -t $CONTAINER_TAG ./container
     # Try again
-    docker run --rm --name $CONTAINER_NAME -itd $CONTAINER_TAG
+    docker run --rm --name $CONTAINER_NAME -td $CONTAINER_TAG
 fi
