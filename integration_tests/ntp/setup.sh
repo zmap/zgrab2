67be4f50f7c4053c92a28f1cb2c6dd6ffa6b47d0
Justin Bastress <jbastress@censys.io>
2018-01-31 16:28:16 -0500

    wait for NTP containers to be ready before exiting setup.sh

--- integration_tests/ntp/setup.sh
+++ integration_tests/ntp/setup.sh
@@ -13,17 +13,30 @@ for version in $versions; do
     DOCKER_RUN_ARGS="--privileged -itd --rm --name $CONTAINER_NAME"
 
     if docker ps --filter "name=$CONTAINER_NAME" | grep -q $CONTAINER_NAME; then
-        echo "ntp/setup: Container $CONTAINER_NAME already running -- stopping..."
-        docker stop $CONTAINER_NAME
-        echo "...stopped."
-    fi
-
-    # First attempt: just launch the container
-    if ! docker run $DOCKER_RUN_ARGS "$CONTAINER_TAG:$version"; then
-        # If it fails, build it from ./container/Dockerfile
-        echo "ntp/setup: Building $CONTAINER_TAG:$version from ./container-$version"
-        docker build -t "$CONTAINER_TAG:$version" ./container-$version
-        # Try again
-        docker run $DOCKER_RUN_ARGS $CONTAINER_TAG:$version
+        echo "ntp/setup: Container $CONTAINER_NAME already running -- skipping."
+    else
+        # First attempt: just launch the container
+        if ! docker run $DOCKER_RUN_ARGS "$CONTAINER_TAG:$version"; then
+            # If it fails, build it from ./container/Dockerfile
+            echo "ntp/setup: Building $CONTAINER_TAG:$version from ./container-$version"
+            docker build -t "$CONTAINER_TAG:$version" ./container-$version
+            # Try again
+            docker run $DOCKER_RUN_ARGS $CONTAINER_TAG:$version
+        fi
     fi
 done
+
+echo -n "ntp/setup: Waiting on zgrab_ntp_openntp..."
+while ! docker logs --tail all zgrab_ntp_openntp | grep -q "ntp engine ready"; do
+    echo -n "."
+done
+echo "...done."
+
+echo -n "ntp/setup: Waiting on zgrab_ntp_4.2.6..."
+while ! docker logs --tail all zgrab_ntp_4.2.6 | grep -q "^interface_action.*listen$"; do
+    echo -n "."
+done
+
+sleep 1
+
+echo "...done."
