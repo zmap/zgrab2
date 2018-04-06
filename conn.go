a6fe2f82b466f7bd037bf3540c7d3d200425ff8a
Justin Bastress <jbastress@censys.io>
2018-04-06 15:34:25 -0400

    read full banners

--- conn.go
+++ conn.go
@@ -8,12 +8,18 @@ import (
 // TimeoutConnection wraps an existing net.Conn connection, overriding the Read/Write methods to use the configured timeouts
 type TimeoutConnection struct {
 	net.Conn
-	Timeout time.Duration
+	Timeout               time.Duration
+	explicitReadDeadline  bool
+	explicitWriteDeadline bool
+	explicitDeadline      bool
 }
 
 // TimeoutConnection.Read calls Read() on the underlying connection, using any configured deadlines
 func (c *TimeoutConnection) Read(b []byte) (n int, err error) {
-	if c.Timeout > 0 {
+	if c.explicitReadDeadline || c.explicitDeadline {
+		c.explicitReadDeadline = false
+		c.explicitDeadline = false
+	} else if c.Timeout > 0 {
 		if err = c.Conn.SetReadDeadline(time.Now().Add(c.Timeout)); err != nil {
 			return 0, err
 		}
@@ -23,12 +29,54 @@ func (c *TimeoutConnection) Read(b []byte) (n int, err error) {
 
 // TimeoutConnection.Write calls Write() on the underlying connection, using any configured deadlines
 func (c *TimeoutConnection) Write(b []byte) (n int, err error) {
-	if c.Timeout > 0 {
+	if c.explicitWriteDeadline || c.explicitDeadline {
+		c.explicitWriteDeadline = false
+		c.explicitDeadline = false
+	} else if c.Timeout > 0 {
 		if err = c.Conn.SetWriteDeadline(time.Now().Add(c.Timeout)); err != nil {
 			return 0, err
 		}
 	}
 	return c.Conn.Write(b)
+}
+
+// SetReadDeadline sets an explicit ReadDeadline that will override the timeout
+// for one read. Use deadline = 0 to clear the deadline.
+func (c *TimeoutConnection) SetReadDeadline(deadline time.Time) error {
+	if !deadline.IsZero() {
+		err := c.Conn.SetReadDeadline(deadline)
+		if err != nil {
+			return err
+		}
+	}
+	c.explicitReadDeadline = !deadline.IsZero()
+	return nil
+}
+
+// SetWriteDeadline sets an explicit WriteDeadline that will override the
+// WriteDeadline for one write. Use deadline = 0 to clear the deadline.
+func (c *TimeoutConnection) SetWriteDeadline(deadline time.Time) error {
+	if !deadline.IsZero() {
+		err := c.Conn.SetWriteDeadline(deadline)
+		if err != nil {
+			return err
+		}
+	}
+	c.explicitWriteDeadline = deadline.IsZero()
+	return nil
+}
+
+// SetDeadline sets a read / write deadline that will override the deadline for
+// a single read/write. Use deadline = 0 to clear the deadline.
+func (c *TimeoutConnection) SetDeadline(deadline time.Time) error {
+	if !deadline.IsZero() {
+		err := c.Conn.SetDeadline(deadline)
+		if err != nil {
+			return err
+		}
+	}
+	c.explicitDeadline = deadline.IsZero()
+	return nil
 }
 
 // GetTimeoutDialer returns a Dialer function that dials with the given timeout
