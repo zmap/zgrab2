4060770ddc920f04fd0c8744e4cd2e96c7400a8c
Justin Bastress <jbastress@censys.io>
2018-04-12 11:02:56 -0400

    update zgrab2 schema

--- schemas/zgrab2.py
+++ schemas/zgrab2.py
@@ -14,15 +14,25 @@ import schemas.zcrypto as zcrypto
 # TODO: Can this be somehow replaced with the main registry?
 scan_response_types = {}
 
+
 # Placeholder / RFU. Many mysql fields are only included in debug mode.
 def DebugOnly(childType):
     return childType
 
+
+# Get a dict of all keys, mapping key -> true
+def FlagsSet(keys, **kwargs):
+    return SubRecord({
+        key: Boolean() for key in keys
+    }, **kwargs)
+
+
 # zgrab2/processing.go: Grab
 grab_result = Record({
-    "ip": IPv4Address(required = False),
-    "domain": String(required = False),
-    "data": SubRecord(scan_response_types, required = True),
+    # TODO: ip may be required; see https://github.com/zmap/zgrab2/issues/104
+    "ip": IPv4Address(required=False, doc="The IP address of the target."),
+    "domain": String(required=False, doc="The domain name of the target, if available."),
+    "data": SubRecord(scan_response_types, required=True, doc="The scan data for this host."),
 })
 
 # zgrab2/module.go: const SCAN_*
@@ -34,28 +44,29 @@ STATUS_VALUES = [
   "io-timeout",
   "protocol-error",
   "application-error",
-  "unknown-error"
+  "unknown-error",
 ]
 
 # zgrab2/module.go: ScanResponse
 base_scan_response = SubRecord({
-    "status": Enum(values = STATUS_VALUES, required = True),
-    "protocol": String(required = True),
-    "timestamp": DateTime(required = True),
-    "result": SubRecord({}, required = False), # This is overridden by the protocols' implementations
-    "error": String(required = False)
+    "status": Enum(values=STATUS_VALUES, required=True, doc="The status of the request."),
+    "protocol": String(required=True, doc="The identifier of the protocol being scanned."),
+    "timestamp": DateTime(required=True, doc="The time the scan was started."),
+    "result": SubRecord({}, required=False),  # This is overridden by the protocols' implementations
+    "error": String(required=False, doc="If the status was not success, error may contain information about the failure.")
     # TODO: error_component? domain?
 })
 
 # zgrab2/tls.go: TLSLog
 tls_log = SubRecord({
-    "handshake_log": zcrypto.tls_handshake,
-    "heartbleed_log": zcrypto.heartbleed_log
+    "handshake_log": zcrypto.tls_handshake.new(doc="The TLS handshake log."),
+    "heartbleed_log": zcrypto.heartbleed_log.new(doc="The heartbleed scan log, if heartbleed scanning was enabled; otherwise, absent.")
 })
+
 
 # Register a schema type for responses with the given name.
 def register_scan_response_type(name, schema):
     scan_response_types[name] = schema
 
-zschema.registry.register_schema("zgrab2", grab_result)
 
+zschema.registry.register_schema("zgrab2", grab_result)
