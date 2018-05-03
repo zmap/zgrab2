001be81fdd642fd98a1bd3b75783c37e531aae0f
ajholland <ajholland77@yahoo.com>
2018-05-03 16:40:17 -0400

    Fixup integration tests, change default and description

--- module.go
+++ module.go
@@ -62,7 +62,7 @@ type ScanFlags interface {
 type BaseFlags struct {
 	Port    uint          `short:"p" long:"port" description:"Specify port to grab on"`
 	Name    string        `short:"n" long:"name" description:"Specify name for output json, only necessary if scanning multiple modules"`
-	Timeout time.Duration `short:"t" long:"timeout" description:"Set connection timeout in seconds (0 = no timeout)" default:"10"`
+	Timeout time.Duration `short:"t" long:"timeout" description:"Set connection timeout (0 = no timeout)" default:"10s"`
 }
 
 // UDPFlags contains the common options used for all UDP scans
