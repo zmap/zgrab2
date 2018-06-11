e46f988d28c4f4d5b07d7e0338ea2f9f5f2df43d
Clayton Zimmerman <clayzim118@gmail.com>
2018-06-11 16:52:26 -0400

    Configures docker container to run scanner correctly.

--- integration_tests/ipp/container/entrypoint.sh
+++ integration_tests/ipp/container/entrypoint.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 
-echo "ERROR: ***"
-echo "ERROR: This container's entrypoint must be customized."
-echo "ERROR: See integration_tests/ipp/container/*"
-echo "FIXME: ***"
-exit 1
-
-# This should do the same work as the init.d start script, but the 
-# process should run in the foreground.
-# In some cases, it may be appropriate to exit (and hence stop the 
-# container) if the daemon process ends, but in others that may be
-# expected behavior.
-
 set -x
 
 while true; do
 #FIXME: Determine whether -f or -F is ideal, and whether any other options are needed
-  if ! /usr/sbin/cupsd -f --fixme-service-options; then
+  if ! /usr/sbin/cupsd -f; then
     echo "cupsd exited unexpectedly. Restarting..."
     sleep 1
   fi
--- integration_tests/ipp/test.sh
+++ integration_tests/ipp/test.sh
@@ -11,7 +11,7 @@ CONTAINER_NAME=zgrab_ipp
 
 OUTPUT_FILE=$ZGRAB_OUTPUT/ipp/ipp.json
 
-echo "ipp/test: Tests runner for ipp"
+echo "ipp/test: Testing IPP on $CONTAINER_NAME..."
 # TODO FIXME: Add any necessary flags or additional tests
 CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh ipp > $OUTPUT_FILE
 # TODO: Add version with TLS flag when that's implemented
@@ -22,14 +22,15 @@ docker logs --tail all $CONTAINER_NAME
 echo ")}] END docker logs from $CONTAINER_NAME"
 
 # TODO: If there are any other relevant log files, dump those to stdout here.
-echo "ipp/test: BEGIN cups logs from $CONTAINER_NAME [{("
-docker exec -t $CONTAINER_NAME cat //var/log/cups/access_log
-echo ")}] END cups logs from $CONTAINER_NAME"
+# FIXME: Only dump these logs if they exist
+#echo "ipp/test: BEGIN cups logs from $CONTAINER_NAME [{("
+#docker exec -t $CONTAINER_NAME cat //var/log/cups/access_log
+#echo ")}] END cups logs from $CONTAINER_NAME"
 
-echo "ipp/test: BEGIN cups logs from $CONTAINER_NAME [{("
-docker exec -t $CONTAINER_NAME cat //var/log/cups/error_log
-echo ")}] END cups logs from $CONTAINER_NAME"
+#echo "ipp/test: BEGIN cups logs from $CONTAINER_NAME [{("
+#docker exec -t $CONTAINER_NAME cat //var/log/cups/error_log
+#echo ")}] END cups logs from $CONTAINER_NAME"
 
-echo "ipp/test: BEGIN cups logs from $CONTAINER_NAME [{("
-docker exec -t $CONTAINER_NAME cat //var/log/cups/page_log
-echo ")}] END cups logs from $CONTAINER_NAME"
+#echo "ipp/test: BEGIN cups logs from $CONTAINER_NAME [{("
+#docker exec -t $CONTAINER_NAME cat //var/log/cups/page_log
+#echo ")}] END cups logs from $CONTAINER_NAME"
