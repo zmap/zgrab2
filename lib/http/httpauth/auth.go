446b24d0859fcdc0e992f18c0f89db096f52f020
Clayton Zimmerman <clayzim118@gmail.com>
2018-08-14 16:15:34 -0400

    Refactors authenticator into one struct which can call getBasicAuth or getDigestAuth based on what's seen in the 'Www-Authenticate' header.

--- lib/http/httpauth/auth.go
+++ lib/http/httpauth/auth.go
@@ -18,37 +18,25 @@ import (
 
 type Authenticator interface {
 	TryGetAuth(req *http.Request, resp *http.Response) string
-	setAuth(host string, userpass *credential)
 }
 
 // TODO: Make this contain state useful for constructing a next response (ie: nextnonce field)
 // TODO: Session state ("-sess") could also be handy here, since it persists from one request to the next.
-		// Though that might also be wrong, since this object should persist while connecting to one host or another.
+	// Though that might also be wrong, since this object should persist while connecting to one host or another.
 // TODO: Similarly, maintaining nonce counter presents some interesting challenges. Maybe more state per-host makes sense.
-type digestAuthenticator map[string]*credential
-
-// Map from hosts to credential pointers. Shouldn't be accessed directly.
-type basicAuthenticator map[string]*credential
+type authenticator struct {
+	// Map from hosts to credential pointers. Shouldn't be accessed directly.
+	creds map[string]*credential
+}
 
 // TODO: Actually explain this.
 type credential struct {
 	Username, Password string
 }
 
-func NewDigestAuthenticator(credsFilename string, hostsToCreds map[string]string) (digestAuthenticator, error) {
-	auther := make(digestAuthenticator)
-	err := initializeAuthenticator(auther, credsFilename, hostsToCreds)
-	return auther, err
-}
-
-func NewBasicAuthenticator(credsFilename string, hostsToCreds map[string]string) (basicAuthenticator, error) {
-	auther := make(basicAuthenticator)
-	err := initializeAuthenticator(auther, credsFilename, hostsToCreds)
-	return auther, err
-}
-
 // TODO: Make sure that you can only specify one file? Maybe supporting multiple files makes sense.
-func initializeAuthenticator(auther Authenticator, credsFilename string, hostsToCreds map[string]string) error {
+func NewAuthenticator(credsFilename string, hostsToCreds map[string]string) (authenticator, error) {
+	auther := authenticator{creds: make(map[string]*credential)}
 	var err error
 	// If a filename is given, record all {host, username:password} pairs it specifies.
 	if credsFilename != "" {
@@ -62,7 +50,7 @@ func initializeAuthenticator(auther Authenticator, credsFilename string, hostsTo
 	if hostsToCreds != nil {
 		populate(auther, hostsToCreds)
 	}
-	return err
+	return auther, err
 }
 
 func readCreds(filename string) (map[string]string, error) {
@@ -100,7 +88,7 @@ func readCreds(filename string) (map[string]string, error) {
 // TODO: Determine whether an input that doesn't specify host should be assumed to default to all hosts
 // Subsequent calls to populate (only made from NewAuthenticator) will, if possible,
 // overwrite the result of previous calls.
-func populate(result Authenticator, hostsToCreds map[string]string) {
+func populate(result authenticator, hostsToCreds map[string]string) {
 	for host, userpass := range hostsToCreds {
 		creds := strings.Split(userpass, ":")
 		user := creds[0]
@@ -109,7 +97,7 @@ func populate(result Authenticator, hostsToCreds map[string]string) {
 		if len(creds) > 1 {
 			pass = strings.Join(creds[1:], ":")
 		}
-		result.setAuth(host, &credential{Username: user, Password: pass})
+		result.creds[host] = &credential{Username: user, Password: pass}
 	}
 }
 
@@ -230,130 +218,142 @@ func generateClientNonce() (string, error) {
 // TODO: This function totally needs to be split up into constituent parts.
 // TODO: Determine whether these args need to be pointers at all? Efficiency is a real contributor to that.
 // TODO: Figure out whether a method signature that doesn't rely on http for types is better or more versatile
-func (credentials digestAuthenticator) TryGetAuth(req *http.Request, resp *http.Response) string {
+func getDigestAuth(creds *credential, req *http.Request, resp *http.Response) string {
 	// Return quickly in the case that Authorization header can't be constructed
 	if resp == nil || resp.Header == nil {
 		return ""
 	}
 
-	host := getHost(req)
-	// TODO: Maybe act differently if host is empty
-	creds, ok := credentials[host]
-	if ok {
-		// TODO: Add an option to work with Proxy-Authenticate header (maybe just take in headers overall?)
-		// TODO: Make sure Get works correctly for this header name
-		// TODO: Make parse (creating params) or accessing params canonicalize param names to all lower-case
-		params := parseWwwAuth(resp.Header.Get("Www-Authenticate"))
-		// Default to MD5 if algorithm isn't specified in response. TODO: Cite RFC for this
-		algoString := valueOrDefault(params["algorithm"], "MD5")
-		var sess bool
-		// Strip "-sess" from algoString if present
-		if strings.HasSuffix(algoString, "-sess") {
-			sess = true
-			// This assumes that the algorithm value "-sess" will never be specified, and it shouldn't
-			algoString = algoString[:len(algoString)-5]
-		}
-		var algo func(string) string
-		if algo = algorithms[algoString]; algo == nil {
-			// Full failure if algorithm can't be resolved, since you just can't continue.
-			return ""
-		}
-
-		realm := params["realm"]
-		nonce := params["nonce"]
-		cnonce, err := generateClientNonce()
-		if err != nil {
-			// Refuse to continue if a client nonce can't be generated
-			return ""
-		}
-
-		// RFC 7616 Section 3.4.2 https://tools.ietf.org/html/rfc7616#section-3.4.2
-		var a1 string
-		a1Components := []string{unquote(creds.Username), unquote(realm), creds.Password}
-		if sess {
-			hash := algo(strings.Join(a1Components, ":"))
-			a1Components = []string{hash, unquote(nonce), unquote(cnonce)}
-			a1 = strings.Join(a1Components, ":")
-		} else {
-			a1 = strings.Join(a1Components, ":")
-		}
-
-		// According to request.go: "For client requests an empty [method] string means GET."
-		method := valueOrDefault(req.Method, "GET")
-		// TODO: Figure out whether there are circumstances in which this wouldn't work to get URI
-		requestURI := strings.Join(strings.Split(req.URL.String(), host)[1:], host)
-		qop := valueOrDefault(params["qop"], "auth")
-		qop = strings.Split(qop, ", ")[0]
-		// Restores end quote if it was cut off due to truncating a list of values.
-		if qop[len(qop)-1:] != `"` {
-			qop += `"`
-		}
-		// RFC 7616 Section 3.4.3 https://tools.ietf.org/html/rfc7616#section-3.4.3
-		var a2 string
-		a2Components := []string{method, requestURI}
-		if qop == "auth-int" {
-			// TODO: Determine whether this check is actually necessary.
-			if req.Body == nil {
-				return ""
-			}
-			bodyText, err := ioutil.ReadAll(req.Body)
-			// TODO: Actually error correctly
-			if err != nil {
-				fmt.Println(err)
-				return ""
-			}
-			entityBody := string(bodyText)
-
-			a2Components = append(a2Components, algo(entityBody))
-			a2 = strings.Join(a2Components, ":")
-		} else {
-			// Execute if qop is "auth" or unspecified
-			a2 = strings.Join(a2Components, ":")
-		}
-
-		// TODO: Stop hard-coding nonceCount (nc) as 1. Somehow keep track of that.
-		nc := fmt.Sprintf("%08x", 1)
-
-		dataComponents := []string{unquote(nonce), nc, unquote(cnonce), unquote(qop), algo(a2)}
-		response := `"` + keyedDigest(algo, algo(a1), strings.Join(dataComponents, ":")) + `"`
-
-		// Username must be hashes after any other hashing, per RFC 7616 Section 3.4.4
-		// TODO: Write logic that determines whether to include username or username*, how to encode that
-		// TODO: If the username would necessitate using username*, but hashing is enabled, no * is required. Just send the hash.
-		username := creds.Username
-		userhash := valueOrDefault(params["userhash"], "false")
-		if userhash == "true" {
-			username = algo(unquote(username) + ":" + unquote(realm))
-		}
-
-		return "Digest opaque=" + params["opaque"] + ", algorithm=" + algoString + ", response=" + response + ", username=\"" + username + "\", realm=" + realm + ", uri=\"" + requestURI + "\", qop=" + qop + ", cnonce=\"" + cnonce + "\", nc=" + nc + ", userhash=" + userhash
+	// TODO: Add an option to work with Proxy-Authenticate header (maybe just take in headers overall?)
+	// TODO: Make sure Get works correctly for this header name
+	// TODO: Make parse (creating params) or accessing params canonicalize param names to all lower-case
+	params := parseWwwAuth(resp.Header.Get("Www-Authenticate"))
+	// Default to MD5 if algorithm isn't specified in response. TODO: Cite RFC for this
+	algoString := valueOrDefault(params["algorithm"], "MD5")
+	var sess bool
+	// Strip "-sess" from algoString if present
+	if strings.HasSuffix(algoString, "-sess") {
+		sess = true
+		// This assumes that the algorithm value "-sess" will never be specified, and it shouldn't
+		algoString = algoString[:len(algoString)-5]
 	}
-	// TODO: Otherwise, assign default creds if those are specified
-	return ""
+	var algo func(string) string
+	if algo = algorithms[algoString]; algo == nil {
+		// Full failure if algorithm can't be resolved, since you just can't continue.
+		return ""
+	}
+
+	realm := params["realm"]
+	nonce := params["nonce"]
+	cnonce, err := generateClientNonce()
+	if err != nil {
+		// Refuse to continue if a client nonce can't be generated
+		return ""
+	}
+
+	// RFC 7616 Section 3.4.2 https://tools.ietf.org/html/rfc7616#section-3.4.2
+	var a1 string
+	a1Components := []string{unquote(creds.Username), unquote(realm), creds.Password}
+	if sess {
+		hash := algo(strings.Join(a1Components, ":"))
+		a1Components = []string{hash, unquote(nonce), unquote(cnonce)}
+		a1 = strings.Join(a1Components, ":")
+	} else {
+		a1 = strings.Join(a1Components, ":")
+	}
+
+	// According to request.go: "For client requests an empty [method] string means GET."
+	method := valueOrDefault(req.Method, "GET")
+	host := getHost(req)
+	// TODO: Use specified requestURI if available.
+	// TODO: Figure out whether there are circumstances in which this wouldn't work to get URI
+	requestURI := strings.Join(strings.Split(req.URL.String(), host)[1:], host)
+	qop := valueOrDefault(params["qop"], "auth")
+	qop = strings.Split(qop, ", ")[0]
+	// Restores end quote if it was cut off due to truncating a list of values.
+	if qop[len(qop)-1:] != `"` {
+		qop += `"`
+	}
+	// RFC 7616 Section 3.4.3 https://tools.ietf.org/html/rfc7616#section-3.4.3
+	var a2 string
+	a2Components := []string{method, requestURI}
+	if qop == "auth-int" {
+		// TODO: Determine whether this check is actually necessary.
+		if req.Body == nil {
+			return ""
+		}
+		bodyText, err := ioutil.ReadAll(req.Body)
+		// TODO: Actually error correctly
+		if err != nil {
+			fmt.Println(err)
+			return ""
+		}
+		entityBody := string(bodyText)
+
+		a2Components = append(a2Components, algo(entityBody))
+		a2 = strings.Join(a2Components, ":")
+	} else {
+		// Execute if qop is "auth" or unspecified
+		a2 = strings.Join(a2Components, ":")
+	}
+
+	// TODO: Stop hard-coding nonceCount (nc) as 1. Somehow keep track of that.
+	nc := fmt.Sprintf("%08x", 1)
+
+	dataComponents := []string{unquote(nonce), nc, unquote(cnonce), unquote(qop), algo(a2)}
+	response := `"` + keyedDigest(algo, algo(a1), strings.Join(dataComponents, ":")) + `"`
+
+	// Username must be hashes after any other hashing, per RFC 7616 Section 3.4.4
+	// TODO: Write logic that determines whether to include username or username*, how to encode that
+	// TODO: If the username would necessitate using username*, but hashing is enabled, no * is required. Just send the hash.
+	username := creds.Username
+	userhash := valueOrDefault(params["userhash"], "false")
+	if userhash == "true" {
+		username = algo(unquote(username) + ":" + unquote(realm))
+	}
+
+	return "Digest opaque=" + params["opaque"] + ", algorithm=" + algoString + ", response=" + response + ", username=\"" + username + "\", realm=" + realm + ", uri=\"" + requestURI + "\", qop=" + qop + ", cnonce=\"" + cnonce + "\", nc=" + nc + ", userhash=" + userhash
+}
+
+func getBasicAuth(creds *credential) string {
+	// Explicitly declare Header so that it's a non-nil map that can be assigned to in .SetBasicAuth
+	temp := &http.Request{Header: make(http.Header)}
+	temp.SetBasicAuth(creds.Username, creds.Password)
+	return temp.Header.Get("Authorization")
 }
 
 // TODO: Determine whether these args need to be pointers at all? Efficiency is a real contributor to that.
 // TODO: Figure out whether a method signature that doesn't rely on http for types is better or more versatile
-func (credentials basicAuthenticator) TryGetAuth(req *http.Request, resp *http.Response) string {
+// TODO: Really nail down what the correct policy is here.
+	// 1) There can be a header or not
+	// 2) There can be credentials for a host or not
+	// 3) Header can contain scheme that's known, unknown, or none
+	// 3) A specified scheme can be "Basic" or "Digest"
+// TODO: Invert this so that resp is checked before presence of host
+func (auther authenticator) TryGetAuth(req *http.Request, resp *http.Response) string {
 	host := getHost(req)
-	// Explicitly declare Header so that it's a non-nil map that can be assigned to in .SetBasicAuth
-	temp := &http.Request{Header: make(http.Header)}
+	creds, ok := auther.creds[host]
 	// TODO: Maybe act differently if host is empty
-	creds, ok := credentials[host]
+	// Credentials were found for the relevant host
 	if ok {
-		temp.SetBasicAuth(creds.Username, creds.Password)
+		// Response Header exists
+		if resp != nil && resp.Header != nil {
+			scheme := strings.Split(resp.Header.Get("Www-Authenticate"), " ")[0]
+			switch scheme {
+			case "Basic":
+				return getBasicAuth(creds)
+			case "Digest":
+				return getDigestAuth(creds, req, resp)
+			default:
+				return ""
+			}
+		} else {
+			// Guess BasicAuth, avoiding wait for 2nd response if correct
+			return getBasicAuth(creds)
+		}
 	}
 	// TODO: Otherwise, assign default creds if those are specified
-
-	return temp.Header.Get("Authorization")
-}
-
-func (credentials digestAuthenticator) setAuth(host string, userpass *credential) {
-	credentials[host] = userpass
-}
-
-func (credentials basicAuthenticator) setAuth(host string, userpass *credential) {
-	credentials[host] = userpass
+	return ""
 }
 
 // TODO: Handle discrepencies between hostname and ip address
