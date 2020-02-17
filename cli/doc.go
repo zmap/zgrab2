2af2f8122ffe7395339415a6649569507ec8fb01
David Adrian <davidcadrian@gmail.com>
2020-02-17 16:28:57 -0500

    Replace AddCommand with ModuleSet and imports

---
+++ cli/doc.go
@@ -0,0 +1,4 @@
+// Package cli contains structures useful for building ZGrab2 CLI interfaces. It
+// can be imported by "github.com/zmap/zgrab2". It must not import any parent or
+// sibling packages.
+package cli
