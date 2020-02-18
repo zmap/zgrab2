d1d22cb506755455d4d9fde0ab55d59ed02ec810
David Adrian <davidcadrian@gmail.com>
2020-02-18 14:23:22 -0500

    Use new OutputFlags in replacement main.go

--- environment.go
+++ environment.go
@@ -1,8 +1,11 @@
 package zgrab2
 
 import (
+	"bufio"
 	"io"
 	"sync"
+
+	"github.com/sirupsen/logrus"
 )
 
 type InputSource struct {
@@ -17,37 +20,48 @@ type OutputDestination struct {
 	w io.Writer
 }
 
-func (out *OutputDestination) WriteOutputsFromChannel() {
-
+// WriteOutputsFromChannel reads from the provided channel and writes the output
+// via the configured writer.
+func (out *OutputDestination) WriteOutputsFromChannel(resultsChannel <-chan []byte) {
+	buf := bufio.NewWriter(out.w)
+	OutputResults(buf, resultsChannel)
 }
 
 type Environment struct {
 	Input  *InputSource
 	Output *OutputDestination
+	Logger *logrus.Logger
 	sync.WaitGroup
 
-	scanTargetChannel chan ScanTarget
+	scanTargetChannel     chan ScanTarget
+	encodedResultsChannel chan []byte
 }
 
 func (env *Environment) Start() {
 	env.scanTargetChannel = make(chan ScanTarget)
+	env.encodedResultsChannel = make(chan []byte)
 	env.Add(2)
 	go env.ReadInput()
 	go env.WriteOutput()
 	go func() {
-		for range env.scanTargetChannel {
+		for t := range env.scanTargetChannel {
 			// Scanning not yet implemented
+			env.Logger.Debugf("received scan target: %v", t)
 		}
+		env.Logger.Debug("finished scanning, closing output channel")
+		close(env.encodedResultsChannel)
 	}()
 }
 
 func (env *Environment) WriteOutput() {
-	env.Output.WriteOutputsFromChannel()
 	defer env.Done()
+	env.Output.WriteOutputsFromChannel(env.encodedResultsChannel)
+	env.Logger.Debug("finished writing output")
 }
 
 func (env *Environment) ReadInput() {
 	defer env.Done()
 	env.Input.ReadInputsToChannel(env.scanTargetChannel)
+	env.Logger.Debug("finished reading input, closing scan target channel")
 	close(env.scanTargetChannel)
 }
