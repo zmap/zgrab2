b68e50336002ec61eaf79aa28314e1bfb26a1e52
Elliot Cubit <elliot@censys.io>
2020-05-21 11:55:04 -0400

    Adds verification step for POP3 banners

--- modules/pop3/pop3.go
+++ modules/pop3/pop3.go
@@ -1,8 +1,14 @@
+// Scanner for POP3 protocol
+// https://www.ietf.org/rfc/rfc1939.txt
+
 package pop3
 
 import (
 	"net"
 	"regexp"
+	"errors"
+	"strings"
+	"io"
 
 	"github.com/zmap/zgrab2"
 )
@@ -17,15 +23,34 @@ type Connection struct {
 	Conn net.Conn
 }
 
+// Verifies that a POP3 banner begins with a valid status indicator
+func VerifyPOP3Contents(n int, ret []byte) (string, error) {
+	s := string(ret[0:n])
+	iword := strings.Index(s, " ")
+	if iword > 2 {
+		subst := s[:iword]
+		if subst == "+OK" {
+			return s, nil
+		}
+		if subst == "+ERR" {
+			return s, zgrab2.NewScanError(zgrab2.SCAN_APPLICATION_ERROR,
+																		errors.New("POP3 Reported Error"))
+		}
+	}
+	return s, zgrab2.NewScanError(zgrab2.SCAN_PROTOCOL_ERROR,
+																errors.New("Invalid response for POP3"))
+}
+
 // ReadResponse reads from the connection until it matches the pop3EndRegex. Copied from the original zgrab.
-// TODO: Catch corner cases, parse out success/error character.
+// TODO: Catch corner cases
 func (conn *Connection) ReadResponse() (string, error) {
 	ret := make([]byte, readBufferSize)
 	n, err := zgrab2.ReadUntilRegex(conn.Conn, ret, pop3EndRegex)
-	if err != nil {
-		return "", nil
+	// Don't quit for timeouts since we might have gotten relevant data still
+	if err != nil && err != io.EOF && !zgrab2.IsTimeoutError(err) {
+		return "", err
 	}
-	return string(ret[0:n]), nil
+	return VerifyPOP3Contents(n, ret)
 }
 
 // SendCommand sends a command, followed by a CRLF, then wait for / read the server's response.
