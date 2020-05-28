532a4969575faaf99fdc203f5b1b07cc31972bb4
Elliot Cubit <elliot@censys.io>
2020-05-28 12:31:05 -0400

    Fix dropping SCAN_APPLICATION_ERROR in IMAP

--- modules/imap/imap.go
+++ modules/imap/imap.go
@@ -3,8 +3,6 @@ package imap
 import (
 	"net"
 	"regexp"
-	"strings"
-	"errors"
 	"io"
 
 	"github.com/zmap/zgrab2"
@@ -20,21 +18,6 @@ type Connection struct {
 	Conn net.Conn
 }
 
-// Verify banner begins with a valid IMAP response and handle it
-func VerifyIMAPContents(n int, ret []byte) (string, error) {
-	s := string(ret[:n])
-	if strings.HasPrefix(s, "* OK"){
-		return s, nil
-	}
-	if strings.HasPrefix(s, "* NO"){
-		return s, zgrab2.NewScanError(zgrab2.SCAN_APPLICATION_ERROR, errors.New("IMAP reported error"))
-	}
-	if strings.HasPrefix(s, "* BAD"){
-		return s, zgrab2.NewScanError(zgrab2.SCAN_UNKNOWN_ERROR, errors.New("IMAP request was malformed"))
-	}
-	return s, zgrab2.NewScanError(zgrab2.SCAN_PROTOCOL_ERROR, errors.New("Invalid response for IMAP"))
-}
-
 // ReadResponse reads from the connection until it matches the imapEndRegex. Copied from the original zgrab.
 // TODO: Catch corner cases
 func (conn *Connection) ReadResponse() (string, error) {
@@ -43,7 +26,7 @@ func (conn *Connection) ReadResponse() (string, error) {
 	if err != nil && err != io.EOF && !zgrab2.IsTimeoutError(err) {
 		return "", err
 	}
-	return VerifyIMAPContents(n, ret)
+	return string(ret[:n]), nil
 }
 
 // SendCommand sends a command, followed by a CRLF, then wait for / read the server's response.
--- modules/imap/scanner.go
+++ modules/imap/scanner.go
@@ -24,6 +24,7 @@ package imap
 
 import (
 	"fmt"
+	"errors"
 
 	"strings"
 
@@ -148,6 +149,21 @@ func getIMAPError(response string) error {
 	return fmt.Errorf("error: %s", response)
 }
 
+// Check the contents of the IMAP banner and return a relevant ScanStatus
+func VerifyIMAPContents(banner string) zgrab2.ScanStatus {
+	switch {
+	case strings.HasPrefix(banner, "* OK"),
+	     strings.HasPrefix(banner, "* PREAUTH"),
+	     strings.HasPrefix(banner, "* BYE"):
+			return zgrab2.SCAN_SUCCESS
+	case strings.HasPrefix(banner, "* NO"),
+	     strings.HasPrefix(banner, "* BAD"):
+			return zgrab2.SCAN_APPLICATION_ERROR
+	default:
+			return zgrab2.SCAN_PROTOCOL_ERROR
+	}
+}
+
 // Scan performs the IMAP scan.
 // 1. Open a TCP connection to the target port (default 143).
 // 2. If --imaps is set, perform a TLS handshake using the command-line
@@ -180,6 +196,12 @@ func (scanner *Scanner) Scan(target zgrab2.ScanTarget) (zgrab2.ScanStatus, inter
 	if err != nil {
 		return zgrab2.TryGetScanStatus(err), nil, err
 	}
+	// Quit early if we didn't get a valid response
+	// OR save a valid scan result for later
+	sr := VerifyIMAPContents(banner)
+	if (sr == zgrab2.SCAN_PROTOCOL_ERROR){
+		return sr, nil, errors.New("Invalid response for IMAP")
+	}
 	result.Banner = banner
 	if scanner.config.StartTLS {
 		ret, err := conn.SendCommand("a001 STARTTLS")
@@ -209,5 +231,5 @@ func (scanner *Scanner) Scan(target zgrab2.ScanTarget) (zgrab2.ScanStatus, inter
 		}
 		result.CLOSE = ret
 	}
-	return zgrab2.SCAN_SUCCESS, result, nil
+	return sr, result, nil
 }
