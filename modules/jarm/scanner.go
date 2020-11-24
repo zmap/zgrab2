005ddb0bb4f5879b99aa61d29e67003735c810a1
Silas Cutler <silas@blacklab.io>
2020-11-24 11:50:16 -0500

    gofmt to clean up code

--- modules/jarm/scanner.go
+++ modules/jarm/scanner.go
@@ -3,51 +3,47 @@
 package jarm
 
 import (
-    "io"
-    "log"
-    "net"
-    _"fmt"
+	_ "fmt"
+	jarm "github.com/RumbleDiscovery/jarm-go"
+	"github.com/zmap/zgrab2"
+	"io"
+	"log"
+	"net"
 	"strings"
-    "github.com/zmap/zgrab2"
-    jarm "github.com/RumbleDiscovery/jarm-go"
-
 )
 
 // Flags give the command-line flags for the banner module.
 type Flags struct {
-    zgrab2.BaseFlags
-    MaxTries int    `long:"max-tries" default:"1" description:"Number of tries for timeouts and connection errors before giving up."`
+	zgrab2.BaseFlags
+	MaxTries int `long:"max-tries" default:"1" description:"Number of tries for timeouts and connection errors before giving up."`
 }
 
 // Module is the implementation of the zgrab2.Module interface.
 type Module struct {
 }
 
-
 // Scanner is the implementation of the zgrab2.Scanner interface.
 type Scanner struct {
-    config *Flags
+	config *Flags
 }
 
-
 type Results struct {
-    Fingerprint string `json:"fingerprint"`
-    error string `json:"error,omitempty"`
-
+	Fingerprint string `json:"fingerprint"`
+	error       string `json:"error,omitempty"`
 }
 
 // RegisterModule is called by modules/banner.go to register the scanner.
 func RegisterModule() {
-    var module Module
-    _, err := zgrab2.AddCommand("jarm", "jarm",module.Description(), 443, &module)
-    if err != nil {
-        log.Fatal(err)
-    }
+	var module Module
+	_, err := zgrab2.AddCommand("jarm", "jarm", module.Description(), 443, &module)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // NewFlags returns a new default flags object.
 func (m *Module) NewFlags() interface{} {
-    return new(Flags)
+	return new(Flags)
 }
 
 // Description returns an overview of this module.
@@ -55,54 +51,52 @@ func (module *Module) Description() string {
 	return "Send TLS requiests and generate a JARM fingerprint"
 }
 
-
 // GetName returns the Scanner name defined in the Flags.
 func (scanner *Scanner) GetName() string {
-    return scanner.config.Name
+	return scanner.config.Name
 }
 
 // GetPort returns the port being scanned.
 func (scanner *Scanner) GetPort() uint {
-        return scanner.config.Port
+	return scanner.config.Port
 }
 
 // GetTrigger returns the Trigger defined in the Flags.
 func (scanner *Scanner) GetTrigger() string {
-    return scanner.config.Trigger
+	return scanner.config.Trigger
 }
 
 // Protocol returns the protocol identifier of the scan.
 func (scanner *Scanner) Protocol() string {
-    return "jarm"
+	return "jarm"
 }
 
 // InitPerSender initializes the scanner for a given sender.
 func (scanner *Scanner) InitPerSender(senderID int) error {
-    return nil
+	return nil
 }
 
 // NewScanner returns a new Scanner object.
 func (m *Module) NewScanner() zgrab2.Scanner {
-    return new(Scanner)
+	return new(Scanner)
 }
 
 // Validate validates the flags and returns nil on success.
 func (f *Flags) Validate(args []string) error {
-    return nil
+	return nil
 }
 
 // Help returns the module's help string.
 func (f *Flags) Help() string {
-    return ""
+	return ""
 }
 
 // Init initializes the Scanner with the command-line flags.
 func (scanner *Scanner) Init(flags zgrab2.ScanFlags) error {
-    f, _ := flags.(*Flags)
-    scanner.config = f
-    return nil
+	f, _ := flags.(*Flags)
+	scanner.config = f
+	return nil
 }
-
 
 func (scanner *Scanner) Scan(target zgrab2.ScanTarget) (zgrab2.ScanStatus, interface{}, error) {
 	// Stores raw hashes returned from parsing each protocols Hello message
@@ -115,28 +109,28 @@ func (scanner *Scanner) Scan(target zgrab2.ScanTarget) (zgrab2.ScanStatus, inter
 	for _, probe := range jarm.GetProbes(target.Host(), int(scanner.GetPort())) {
 		data := jarm.BuildProbe(probe)
 
-	    var (
-	        conn    net.Conn
-	        err     error
-	        ret []byte
-	    )
-        conn, err = target.Open(&scanner.config.BaseFlags)
-	    if err != nil {
-	        return zgrab2.TryGetScanStatus(err), nil, err
-	    }
-	    defer conn.Close()
+		var (
+			conn net.Conn
+			err  error
+			ret  []byte
+		)
+		conn, err = target.Open(&scanner.config.BaseFlags)
+		if err != nil {
+			return zgrab2.TryGetScanStatus(err), nil, err
+		}
+		defer conn.Close()
 
-        _, err = conn.Write([]byte(data))
-        if err != nil {
-                rawhashes = append(rawhashes, "")
-                continue
-        }
+		_, err = conn.Write([]byte(data))
+		if err != nil {
+			rawhashes = append(rawhashes, "")
+			continue
+		}
 
-        ret, err = zgrab2.ReadAvailable(conn)
-        if err != io.EOF && err != nil {
-                rawhashes = append(rawhashes, "")
-                continue
-        }
+		ret, err = zgrab2.ReadAvailable(conn)
+		if err != io.EOF && err != nil {
+			rawhashes = append(rawhashes, "")
+			continue
+		}
 
 		ans, err := jarm.ParseServerHello(ret, probe)
 		if err != nil {
@@ -144,11 +138,11 @@ func (scanner *Scanner) Scan(target zgrab2.ScanTarget) (zgrab2.ScanStatus, inter
 			continue
 		}
 
-        rawhashes = append(rawhashes, string(ans))
+		rawhashes = append(rawhashes, string(ans))
 
-    }
+	}
 
-    fprint := jarm.RawHashToFuzzyHash(strings.Join(rawhashes, ","))
-    r.Fingerprint = string(fprint)
-    return zgrab2.SCAN_SUCCESS, &r, nil	   
+	fprint := jarm.RawHashToFuzzyHash(strings.Join(rawhashes, ","))
+	r.Fingerprint = string(fprint)
+	return zgrab2.SCAN_SUCCESS, &r, nil
 }
