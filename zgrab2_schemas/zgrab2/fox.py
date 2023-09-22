a735ae8ec9b0773a23d69bbaa18675d5159127ca
Sean Smith <seandanielsmith@gmail.com>
2023-09-21 21:41:27 -0500

    Add TLS to the schema

--- zgrab2_schemas/zgrab2/fox.py
+++ zgrab2_schemas/zgrab2/fox.py
@@ -28,6 +28,7 @@ fox_scan_response = SubRecord({
         'brand_id': String(),
         'sys_info': String(),
         'agent_auth_type': String(),
+        "tls": zgrab2.tls_log,
     })
 }, extends=zgrab2.base_scan_response)
 
