9fe41e6a466402d2e40ccd85c868808839fc8f9e
devStorm <59678453+developStorm@users.noreply.github.com>
2024-05-21 22:02:16 +0000

    patch: template py file validity

--- integration_tests/.template/schema.py
+++ integration_tests/.template/schema.py
@@ -7,12 +7,12 @@ import zschema.registry
 import zcrypto_schemas.zcrypto as zcrypto
 import zgrab2
 
-#{MODULE_NAME}_scan_response = SubRecord({
-    "result": SubRecord({
-        # TODO FIXME IMPLEMENT SCHEMA
-    })
-}, extends=zgrab2.base_scan_response)
+# {MODULE_NAME}_scan_response = SubRecord({
+#     "result": SubRecord({
+#         # TODO FIXME IMPLEMENT SCHEMA
+#     })
+# }, extends=zgrab2.base_scan_response)
 
-zschema.registry.register_schema("zgrab2-#{MODULE_NAME}", #{MODULE_NAME}_scan_response)
+# zschema.registry.register_schema("zgrab2-#{MODULE_NAME}", #{MODULE_NAME}_scan_response)
 
-zgrab2.register_scan_response_type("#{MODULE_NAME}", #{MODULE_NAME}_scan_response)
+# zgrab2.register_scan_response_type("#{MODULE_NAME}", #{MODULE_NAME}_scan_response)
