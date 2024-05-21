29b407123aee1fb25bfecdf05975701f69a7e614
devStorm <59678453+developStorm@users.noreply.github.com>
2024-05-21 23:07:18 +0000

    style: apply python formatter

--- zgrab2_schemas/zgrab2/amqp091.py
+++ zgrab2_schemas/zgrab2/amqp091.py
@@ -7,33 +7,44 @@ import zschema.registry
 from . import zgrab2
 
 # Schema for connectionTune struct
-connection_tune = SubRecord({
-    "channel_max": Unsigned32BitInteger(),
-    "frame_max": Unsigned32BitInteger(),
-    "heartbeat": Unsigned32BitInteger(),
-})
+connection_tune = SubRecord(
+    {
+        "channel_max": Unsigned32BitInteger(),
+        "frame_max": Unsigned32BitInteger(),
+        "heartbeat": Unsigned32BitInteger(),
+    }
+)
 
 # Schema for knownServerProperties struct
-known_server_properties = SubRecord({
-    "product": String(),
-    "version": String(),
-    "platform": String(),
-    "copyright": String(),
-    "information": String(),
-    "unknown_props": String(),
-})
+known_server_properties = SubRecord(
+    {
+        "product": String(),
+        "version": String(),
+        "platform": String(),
+        "copyright": String(),
+        "information": String(),
+        "unknown_props": String(),
+    }
+)
 
 # Schema for Result struct
-result_schema = SubRecord({
-   "result": SubRecord ({ "failure": String(),
-    "version_major": Unsigned32BitInteger(),
-    "version_minor": Unsigned32BitInteger(),
-    "server_properties": known_server_properties,
-    "locales": ListOf(String()),
-    "auth_success": Boolean(),
-    "tune": connection_tune,
-    "tls": zgrab2.tls_log,
-})}, extends=zgrab2.base_scan_response)
+result_schema = SubRecord(
+    {
+        "result": SubRecord(
+            {
+                "failure": String(),
+                "version_major": Unsigned32BitInteger(),
+                "version_minor": Unsigned32BitInteger(),
+                "server_properties": known_server_properties,
+                "locales": ListOf(String()),
+                "auth_success": Boolean(),
+                "tune": connection_tune,
+                "tls": zgrab2.tls_log,
+            }
+        )
+    },
+    extends=zgrab2.base_scan_response,
+)
 
 zschema.registry.register_schema("zgrab2-amqp091", result_schema)
 zgrab2.register_scan_response_type("amqp091", result_schema)
