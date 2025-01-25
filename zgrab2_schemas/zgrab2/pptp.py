585b3f0b5d618b056cbaab98833410964d9fe41c
phillip-stephens <phillip@cs.stanford.edu>
2025-01-25 11:22:35 -0800

    python lint

--- zgrab2_schemas/zgrab2/pptp.py
+++ zgrab2_schemas/zgrab2/pptp.py
@@ -7,14 +7,19 @@ import zschema.registry
 from . import zgrab2
 
 # Schema for ScanResults struct
-pptp_scan_response = SubRecord({
-    "banner": String(),
-    "control_message": String(),
-})
+pptp_scan_response = SubRecord(
+    {
+        "banner": String(),
+        "control_message": String(),
+    }
+)
 
-pptp_scan = SubRecord({
-    "result": pptp_scan_response,
-}, extends=zgrab2.base_scan_response)
+pptp_scan = SubRecord(
+    {
+        "result": pptp_scan_response,
+    },
+    extends=zgrab2.base_scan_response,
+)
 
 zschema.registry.register_schema("zgrab2-pptp", pptp_scan)
 zgrab2.register_scan_response_type("pptp", pptp_scan)
