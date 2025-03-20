3e9c415edbeba7fdeaa59e243e320980b31f776b
phillip-stephens <phillip@cs.stanford.edu>
2025-03-19 21:09:13 -0700

    All SMTP integration tests passing

--- integration_tests/smtp/test.sh
+++ integration_tests/smtp/test.sh
@@ -10,25 +10,35 @@ mkdir -p $ZGRAB_OUTPUT/smtp
 CONTAINER_NAME=zgrab_smtp
 
 OUTPUT_ROOT="$ZGRAB_OUTPUT/smtp"
-versions="helo ehlo-no-starttls ehlo-starttls smtps"
+
+function dumpDockerLogs() {
+  # Dump the docker logs
+  echo "smtp/test: BEGIN docker logs from $CONTAINER_NAME [{("
+  docker logs --tail all $CONTAINER_NAME
+  echo ")}] END docker logs from $CONTAINER_NAME"
+}
 
 function testHelo() {
   CONTAINER_NAME=zgrab_smtp_helo
   CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh smtp > $ZGRAB_OUTPUT/smtp/helo.json
   CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh smtp --send-help > $ZGRAB_OUTPUT/smtp/helo_send_help.json
   CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh smtp --send-quit > $ZGRAB_OUTPUT/smtp/helo_send_quit.json
+  dumpDockerLogs
 }
 
 function testEHLO() {
   CONTAINER_NAME=zgrab_smtp_ehlo_starttls
   CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh smtp > $ZGRAB_OUTPUT/smtp/ehlo_starttls.json
+  dumpDockerLogs
   CONTAINER_NAME=zgrab_smtp_ehlo_no_starttls
   CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh smtp > $ZGRAB_OUTPUT/smtp/ehlo_no_starttls.json
+  dumpDockerLogs
 }
 
 function testSMTPS() {
   CONTAINER_NAME=zgrab_smtp_smtps
   CONTAINER_NAME=$CONTAINER_NAME $ZGRAB_ROOT/docker-runner/docker-run.sh smtp --smtps > $ZGRAB_OUTPUT/smtp/smtps.json
+  dumpDockerLogs
 }
 echo "smtp/test: Tests runner for smtp"
 
@@ -36,24 +46,20 @@ testHelo
 testEHLO
 testSMTPS
 
-#FIELDS="help quit helo ehlo"
-#status=0
-#for field in $FIELDS; do
-#    for file in $(find $OUTPUT_ROOT -iname "*$field*.json"); do
-#        echo "check $file for $field"
-#        RESULT=$(jp data.smtp.result.$field < $file)
-#        if [ "$RESULT" = "null" ]; then
-#            echo "Did not find $field in $file [["
-#            cat $file
-#            echo "]]"
-#            status=1
-#        fi
-#    done
-#done
+FIELDS="help quit helo ehlo"
+status=0
+for field in $FIELDS; do
+    for file in $(find $OUTPUT_ROOT -iname "*$field*.json"); do
+        echo "check $file for $field"
+        RESULT=$(jp data.smtp.result.$field < $file)
+        if [ "$RESULT" = "null" ]; then
+            echo "Did not find $field in $file [["
+            cat $file
+            echo "]]"
+            status=1
+        fi
+    done
+done
 
-## Dump the docker logs
-#echo "smtp/test: BEGIN docker logs from $CONTAINER_NAME [{("
-#docker logs --tail all $CONTAINER_NAME
-#echo ")}] END docker logs from $CONTAINER_NAME"
-#
+
 exit $status
