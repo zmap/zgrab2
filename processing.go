1c4d841c40b684df3c4286c83384aa6b1859c2f8
phillip-stephens <phillip@cs.stanford.edu>
2025-04-03 16:47:05 -0700

    comments

--- processing.go
+++ processing.go
@@ -61,6 +61,7 @@ func (target *ScanTarget) Host() string {
 
 // GetDefaultTCPDialer returns a TCP dialer suitable for modules with default TCP behavior
 func GetDefaultTCPDialer(flags *BaseFlags) func(ctx context.Context, t *ScanTarget, addr string) (net.Conn, error) {
+	// create dialer once and reuse it
 	dialer := GetTimeoutConnectionDialer(flags.Timeout)
 	return func(ctx context.Context, t *ScanTarget, addr string) (net.Conn, error) {
 		// If the scan is for a specific IP, and a domain name is provided, we
@@ -145,6 +146,7 @@ func GetDefaultTLSWrapper(tlsFlags *TLSFlags) func(ctx context.Context, t *ScanT
 
 // GetDefaultUDPDialer returns a UDP dialer suitable for modules with default UDP behavior
 func GetDefaultUDPDialer(flags *BaseFlags) func(ctx context.Context, t *ScanTarget, addr string) (net.Conn, error) {
+	// create dialer once and reuse it
 	dialer := GetTimeoutConnectionDialer(flags.Timeout)
 	return func(ctx context.Context, t *ScanTarget, addr string) (net.Conn, error) {
 		err := dialer.SetRandomLocalAddr("udp", config.localAddrs, config.localPorts)
