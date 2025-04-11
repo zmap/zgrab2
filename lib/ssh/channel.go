f5b21bc4f1adac04bc0bb7d1a496a6b7d6979d7d
phillip-stephens <phillip@cs.stanford.edu>
2025-04-11 13:32:12 -0700

    casting issue

--- lib/ssh/channel.go
+++ lib/ssh/channel.go
@@ -242,7 +242,7 @@ func (ch *channel) WriteExtended(data []byte, extendedCode uint32) (n int, err e
 	ch.writeMu.Unlock()
 
 	for len(data) > 0 {
-		space := min(ch.maxRemotePayload, len(data))
+		space := min(ch.maxRemotePayload, uint32(len(data)))
 		if space, err = ch.remoteWin.reserve(space); err != nil {
 			return n, err
 		}
