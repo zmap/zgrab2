51c4a801f8689f68186ddb3fa626add986b1d21c
phillip-stephens <phillip@cs.stanford.edu>
2025-04-15 16:56:49 -0700

    Init commit for memcached module, adds boilerplate to get an empty module compiling and usable from the CLI

---
+++ integration_tests/memcached/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def test_ascii_telnet():
+    exit(1) # TODO placeholder that will fail CI so we don't forget to implement integration tests
+
+if __name__ == "__main__":
+    test_ascii_telnet()
---
+++ modules/memcached/scanner.go
@@ -0,0 +1,130 @@
+package memcached
+
+// Package memcached provides a zgrab2 module that scans for memcache servers.
+// Default port: 11211 (TCP)
+import (
+	"context"
+	"fmt"
+	"net"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/zmap/zgrab2"
+)
+
+// Flags holds the command-line configuration for the memcached scan module.
+// Populated by the framework.
+type Flags struct {
+	zgrab2.BaseFlags `group:"Basic Options"`
+}
+
+// Module implements the zgrab2.Module interface.
+type Module struct {
+}
+
+// Scanner implements the zgrab2.Scanner interface.
+type Scanner struct {
+	config            *Flags
+	dialerGroupConfig *zgrab2.DialerGroupConfig
+}
+
+// RegisterModule registers the zgrab2 module.
+func RegisterModule() {
+	var module Module
+	_, err := zgrab2.AddCommand("memcached", "memcached", module.Description(), 11211, &module)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// NewFlags returns a default Flags object.
+func (module *Module) NewFlags() any {
+	return new(Flags)
+}
+
+// NewScanner returns a new Scanner instance.
+func (module *Module) NewScanner() zgrab2.Scanner {
+	return new(Scanner)
+}
+
+// Description returns an overview of this module.
+func (module *Module) Description() string {
+	return "Probe for memcached services"
+}
+
+// Validate checks that the flags are valid.
+// On success, returns nil.
+// On failure, returns an error instance describing the error.
+func (flags *Flags) Validate() error {
+	return nil
+}
+
+// Help returns the module's help string.
+func (flags *Flags) Help() string {
+	return ""
+}
+
+// Init initializes the Scanner.
+func (scanner *Scanner) Init(flags zgrab2.ScanFlags) error {
+	f, _ := flags.(*Flags)
+	scanner.config = f
+	scanner.dialerGroupConfig = &zgrab2.DialerGroupConfig{
+		TransportAgnosticDialerProtocol: zgrab2.TransportTCP,
+		BaseFlags:                       &f.BaseFlags,
+	}
+	return nil
+}
+
+// InitPerSender initializes the scanner for a given sender.
+func (scanner *Scanner) InitPerSender(senderID int) error {
+	return nil
+}
+
+// GetName returns the Scanner name defined in the Flags.
+func (scanner *Scanner) GetName() string {
+	return scanner.config.Name
+}
+
+// GetTrigger returns the Trigger defined in the Flags.
+func (scanner *Scanner) GetTrigger() string {
+	return scanner.config.Trigger
+}
+
+// Protocol returns the protocol identifier of the scan.
+func (scanner *Scanner) Protocol() string {
+	return "memcached"
+}
+
+func (scanner *Scanner) GetDialerGroupConfig() *zgrab2.DialerGroupConfig {
+	return scanner.dialerGroupConfig
+}
+
+type MemcachedResult struct {
+	Version         string               `json:"version"`
+	LibeventVersion string               `json:"libevent_version"`
+	SupportsAscii   bool                 `json:"supports_ascii"` // true if the server supports plain-text ASCII protocol
+	Stats           MemcachedResultStats `json:"stats"`
+	// TODO - Add more fields as needed
+}
+
+type MemcachedResultStats struct {
+	PID int `json:"pid"`
+	// TODO - Add fields for memcached stats
+}
+
+// Scan probes for a memcached service.
+// TODO - Describe Scan process
+func (scanner *Scanner) Scan(ctx context.Context, dialGroup *zgrab2.DialerGroup, target *zgrab2.ScanTarget) (zgrab2.ScanStatus, any, error) {
+	conn, err := dialGroup.Dial(ctx, target)
+	if err != nil {
+		return zgrab2.TryGetScanStatus(err), nil, fmt.Errorf("unable to dial target (%s): %w", target.String(), err)
+	}
+	defer func(conn net.Conn) {
+		// cleanup conn
+		zgrab2.CloseConnAndHandleError(conn)
+	}(conn)
+	result := new(MemcachedResult)
+	// TODO - populate memcached result
+
+	return zgrab2.TryGetScanStatus(err), result, err
+}
