6d101ee1112394913c016bb12c8b94029622d3b3
phillip-stephens <phillip@cs.stanford.edu>
2025-05-09 12:13:56 -0400

    fix but in err handling in utility

--- utility.go
+++ utility.go
@@ -155,8 +155,9 @@ func ReadAvailableWithOptions(conn net.Conn, bufferSize int, readTimeout time.Du
 		if err != nil {
 			if IsTimeoutError(err) {
 				err = nil
+			} else {
+				return ret, fmt.Errorf("conn read encountered error after reading %d bytes: %w", n, err)
 			}
-			return ret, fmt.Errorf("conn read encountered error after reading %d bytes: %w", n, err)
 		}
 
 		if n >= maxReadSize {
