9ca1a7915d4729b46f9698330f225ca99796ba82
phillip-stephens <phillip@cs.stanford.edu>
2025-06-24 16:15:04 -0700

    raise default rate limits

--- config.go
+++ config.go
@@ -45,8 +45,8 @@ type Config struct {
 	LocalAddrString      string          `long:"local-addr" description:"Local address(es) to bind to for outgoing connections. Comma-separated list of IP addresses, ranges (inclusive), or CIDR blocks, ex: 1.1.1.1-1.1.1.3, 2.2.2.2, 3.3.3.0/24"`
 	LocalPortString      string          `long:"local-port" description:"Local port(s) to bind to for outgoing connections. Comma-separated list of ports or port ranges (inclusive) ex: 1200-1300,2000"`
 	DNSResolutionTimeout time.Duration   `long:"dns-resolution-timeout" default:"10s" description:"Timeout for DNS resolution of target hostnames. Default is 10 seconds."`
-	DNSServerRateLimit   int             `long:"dns-rate-limit" default:"5000" description:"Rate limit for DNS lookups per second."`
-	ServerRateLimit      int             `long:"server-rate-limit" default:"10" description:"Per-IP rate limit for connections to targets per second."`
+	DNSServerRateLimit   int             `long:"dns-rate-limit" default:"10000" description:"Rate limit for DNS lookups per second."`
+	ServerRateLimit      int             `long:"server-rate-limit" default:"100" description:"Per-IP rate limit for connections to targets per second."`
 	UserIPv4Choice       *bool           `long:"resolve-ipv4" description:"Use IPv4 for resolving domains (accept A records). True by default, use only --resolve-ipv6 for IPv6 only resolution. If used with --resolve-ipv6, will use both IPv4 and IPv6."`
 	UserIPv6Choice       *bool           `long:"resolve-ipv6" description:"Use IPv6 for resolving domains (accept AAAA records). IPv6 is disabled by default. If --resolve-ipv4 is not set and --resolve-ipv6 is, will only use IPv6. If used with --resolve-ipv4, will use both IPv4 and IPv6."`
 	inputFile            *os.File
