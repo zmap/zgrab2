8cef1285baeed4de00058eefd533aa1f48894e9a
phillip-stephens <phillip@cs.stanford.edu>
2025-06-24 15:53:50 -0700

    remove accesses tracking used for developing

--- ratelimit/ratelimit.go
+++ ratelimit/ratelimit.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	lru "github.com/hashicorp/golang-lru/v2/expirable"
 	"golang.org/x/time/rate"
-	"sort"
 	"sync"
 	"time"
 )
@@ -20,17 +19,12 @@ const (
 type PerObjectRateLimiter[K comparable] struct {
 	*sync.Mutex
 	limitLRU *lru.LRU[K, *rate.Limiter]
-	// TODO Phillip remove
-	accesses   map[K]uint // Access counters for each object
-	accessLock sync.Mutex
 }
 
 // NewPerObjectRateLimiter creates a new PerObjectRateLimiter.
 func NewPerObjectRateLimiter[K comparable]() *PerObjectRateLimiter[K] {
 	limiter := &PerObjectRateLimiter[K]{
-		Mutex:      &sync.Mutex{},
-		accesses:   make(map[K]uint),
-		accessLock: sync.Mutex{},
+		Mutex: &sync.Mutex{},
 	}
 	limiter.limitLRU = lru.NewLRU[K, *rate.Limiter](maxLRUSize, nil, maxLRUTTL) // Initialize LRU cache with a maximum size
 	return limiter
@@ -49,46 +43,5 @@ func (l *PerObjectRateLimiter[K]) WaitOrCreate(ctx context.Context, key K, rateL
 	if err := limiter.Wait(ctx); err != nil {
 		return fmt.Errorf("could not wait for rate limiter for key %v: %w", key, err)
 	}
-	l.accessLock.Lock()
-	defer l.accessLock.Unlock()
-	l.accesses[key]++ // initialized in NewPerObjectRateLimit, so safe to assume it exists
 	return nil
-}
-
-func (l *PerObjectRateLimiter[K]) PrintAccesses() string {
-	var result string
-	l.accessLock.Lock()
-	defer l.accessLock.Unlock()
-	totalAccesses := uint(0)
-	// Get access counts, sort in descending order
-	type kv struct {
-		Key   K
-		Value uint
-	}
-	var pairs []kv
-	for k, v := range l.accesses {
-		totalAccesses += v
-		pairs = append(pairs, kv{k, v})
-	}
-
-	// Sort by Value descending
-	sort.Slice(pairs, func(i, j int) bool {
-		return pairs[i].Value > pairs[j].Value
-	})
-	result += fmt.Sprintf("%d Unique IPs\n", len(l.accesses))
-	result += fmt.Sprintf("%d Total accesses\n", totalAccesses)
-	result += fmt.Sprintf("%f Average accesses per IP\n", float64(totalAccesses)/float64(len(l.accesses)))
-
-	// Print sorted result
-	const topK = 20
-	result += fmt.Sprintf("Top %d IPs by Accesses:\n", topK)
-	for i, p := range pairs {
-		if i > topK {
-			// Limit to 20 entries for readability
-			result += "...\n"
-			break
-		}
-		result += fmt.Sprintf("%v - %d\n", p.Key, p.Value)
-	}
-	return result
 }
