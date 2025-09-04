c083b0d6f9c3190be4d8e8c57f8f349c882a0bf6
phillip-stephens <phillip@cs.stanford.edu>
2025-09-04 13:35:12 -0700

    working h2c test

---
+++ http-docker/h2c/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+func main() {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello world")
+	})
+	h2s := &http2.Server{
+		// ...
+	}
+	h1s := &http.Server{
+		Addr:    ":443",
+		Handler: h2c.NewHandler(handler, h2s),
+	}
+	log.Fatal(h1s.ListenAndServe())
+}
