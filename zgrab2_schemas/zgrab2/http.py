895420882749065c5c6743de988205ca14daf1c7
Sean Smith <seandanielsmith@gmail.com>
2025-09-10 14:36:38 -0400

    Add additional tests for http (#606)
    * Add additional tests for http
    
    Add a test for raw headers
    Add a test for scan status
    
    * added headers_raw to zschema for http response
    
    * lint python file
    
    ---------
    
    Co-authored-by: phillip-stephens <phillip@cs.stanford.edu>

--- zgrab2_schemas/zgrab2/http.py
+++ zgrab2_schemas/zgrab2/http.py
@@ -120,6 +120,7 @@ http_response = SubRecord(
         "status_code": Signed32BitInteger(),
         "status_line": String(),
         "headers": http_headers,
+        "headers_raw": String(),
         "body": String(),
         "body_sha256": String(),
     }
@@ -159,6 +160,7 @@ http_response_full = SubRecord(
             }
         ),
         "headers": http_headers,
+        "headers_raw": String(),
         "body": String(),
         "body_sha256": Binary(),
         "content_length": Signed64BitInteger(),
