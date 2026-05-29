04c3cc4063cc4167494f1ebef6c9063adb283717
phillip-stephens <phillip@cs.stanford.edu>
2026-05-29 16:59:50 +1200

    lint checkpoint schema

--- zgrab2_schemas/zgrab2/checkpoint.py
+++ zgrab2_schemas/zgrab2/checkpoint.py
@@ -7,12 +7,10 @@ import zschema.registry
 import zcrypto_schemas.zcrypto as zcrypto
 import zgrab2
 
-checkpoint_scan_response = SubRecord({
-    "result": SubRecord({
-        "firewall_host": String(),
-        "host": String()
-    })
-}, extends=zgrab2.base_scan_response)
+checkpoint_scan_response = SubRecord(
+    {"result": SubRecord({"firewall_host": String(), "host": String()})},
+    extends=zgrab2.base_scan_response,
+)
 
 zschema.registry.register_schema("zgrab2-checkpoint", checkpoint_scan_response)
 
